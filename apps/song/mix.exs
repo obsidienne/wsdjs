8f77bd42d9f6652155d4c041732082caf10e7803
Claudio Bernardes <bernardes.claudio@gmail.com>
2016-11-17 15:31:28 +0100

    Add elixirc_paths

--- apps/song/mix.exs
+++ apps/song/mix.exs
@@ -9,6 +9,7 @@ defmodule Song.Mixfile do
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
      elixir: "~> 1.3",
+     elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      aliases: aliases(),
@@ -22,6 +23,10 @@ defmodule Song.Mixfile do
     [applications: [:logger, :ecto, :postgrex],
      mod: {Song.Application, []}]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
     [{:ecto, "~> 2.0"},
