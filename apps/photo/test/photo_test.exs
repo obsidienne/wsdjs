c55f3ec662dc6d50964433a4ddd468f8a047f7e0
Claudio Bernardes <bernardes.claudio@gmail.com>
2016-12-05 09:06:39 +0100

    Photo changes and tests

--- apps/photo/test/photo_test.exs
+++ apps/photo/test/photo_test.exs
@@ -1,6 +1,5 @@
 defmodule PhotoTest do
   use ExUnit.Case
-  doctest Photo
 
   test "the truth" do
     assert 1 + 1 == 2
--- apps/photo/test/test_helper.exs
+++ apps/photo/test/test_helper.exs
@@ -1,1 +1,3 @@
 ExUnit.start()
+
+Ecto.Adapters.SQL.Sandbox.mode(Photo.Repo, :manual)
