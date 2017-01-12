ec6411325e43fb0cb193eafe3edaeafed5163146
claudio bernardes <bernardes.claudio@gmail.com>
2017-01-13 00:59:50 +0100

    load tops

--- apps/wcsp/priv/repo/seeds.exs
+++ apps/wcsp/priv/repo/seeds.exs
@@ -27,19 +27,39 @@ defmodule Wcsp.Seeds do
       _ ->
         account
     end
-    IO.inspect account
-
     Wcsp.Repo.insert! account
   end
+
+
+  def store_it(row, :tops) do
+    row = row
+    |> Enum.filter(fn {_, v} -> v != "" end)
+    |> Enum.into(%{})
+
+    top = %Wcsp.Top{
+      id: row[:id],
+      account_id: row[:user_id],
+      due_date: Ecto.Date.cast!(row[:due_date]),
+      status: row[:status],
+      inserted_at: Ecto.DateTime.cast!(row[:inserted_at]),
+      updated_at: Ecto.DateTime.cast!(row[:updated_at])
+    }
+
+    Wcsp.Repo.insert! top
+  end
 end
-
-
-account_header = [:id, :email, :admin, :new_song_notification, :user_country, :name, :djname, :avatar_id, :avatar_version, :inserted_at, :updated_at]
 
 #import accounts
 "data/accounts.csv"
 |> Path.expand(__DIR__)
 |> File.stream!
 |> Stream.drop(1)
-|> CSV.decode(headers: account_header, strip_cells: true)
+|> CSV.decode(strip_cells: true, headers: [:id, :email, :admin, :new_song_notification, :user_country, :name, :djname, :avatar_id, :avatar_version, :inserted_at, :updated_at])
 |> Enum.each(&Wcsp.Seeds.store_it(&1, :account))
+
+"data/tops.csv"
+|> Path.expand(__DIR__)
+|> File.stream!
+|> Stream.drop(1)
+|> CSV.decode(strip_cells: true, headers: [:id, :user_id, :inserted_at, :updated_at, :due_date, :status])
+|> Enum.each(&Wcsp.Seeds.store_it(&1, :tops))
