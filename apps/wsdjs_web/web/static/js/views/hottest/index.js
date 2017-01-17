594c9d65bdeda0d51bd71bfed5ae49ff6886319b
claudio bernardes <bernardes.claudio@gmail.com>
2017-01-17 09:18:20 +0100

    menubar working

--- apps/wsdjs_web/web/static/js/views/hottest/index.js
+++ apps/wsdjs_web/web/static/js/views/hottest/index.js
@@ -1,8 +1,12 @@
+import $ from 'jquery';
 import MainView from '../main';
 
 export default class View extends MainView {
   mount() {
     super.mount();
+
+    $("time.timeago").timeago();
+    this.menubar();
 
     // Specific logic here
     console.log('PageNewView mounted');
@@ -13,5 +17,41 @@ export default class View extends MainView {
 
     // Specific logic here
     console.log('PageNewView unmounted');
+  }
+
+  // private function used to show and hide menu from the menubar
+  menubar() {
+    $(document).on("click", function(event){
+      if (!$(event.target).closest('.glsearch').length) {
+         $('.glsearch').removeClass('glsearch--focused');
+         $(".glsearch__results").html("");
+         $("#glsearch").val("");
+       }
+       if (!$(event.target).closest('.glheader__user').length &&
+           !$(event.target).closest('.glsearch').length) {
+         $(".glheader .glheader__menu--open").toggleClass("glheader__menu--open" );
+         $(".glsearch__results").html("");
+         $(".glsearch__results").removeClass("glsearch--focused")
+       }
+    });
+
+    $('#glsearch').on('click', function(e){
+      $('.glsearch__results').addClass('glsearch--focused');
+      $(".glheader .glheader__menu--open").toggleClass("glheader__menu--open" );
+      $('.glsearch').addClass("glsearch--focused");
+    });
+
+    $(function() {
+      $(".glheader__menu-icon").on("click", function(e) {
+        if ($(this).next().hasClass("glheader__menu--open")) {
+          $(this).next().removeClass( "glheader__menu--open" );
+          $('.glsearch--focused').removeClass('glsearch--focused');
+        } else {
+          $('.glsearch--focused').removeClass('glsearch--focused');
+          $(".glheader .glheader__menu--open").toggleClass("glheader__menu--open" );
+          $(this).next().addClass( "glheader__menu--open" );
+        }
+      })
+    });
   }
 }
--- apps/wsdjs_web/web/static/js/views/main.js
+++ apps/wsdjs_web/web/static/js/views/main.js
@@ -1,13 +1,9 @@
 //https://blog.diacode.com/page-specific-javascript-in-phoenix-framework-pt-1
 
-import $ from 'jquery';
-
 export default class MainView {
   mount() {
     // This will be executed when the document loads...
     console.log('MainView mounted');
-    $("time.timeago").timeago();
-
   }
 
   unmount() {
