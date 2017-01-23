7e8647f4023476215418e2758395ff7df4100faf
claudio bernardes <bernardes.claudio@gmail.com>
2017-01-23 18:35:58 +0100

    use timeago.js (drop jquery dependency)

--- apps/wsdjs_web/web/static/js/views/hottest/index.js
+++ apps/wsdjs_web/web/static/js/views/hottest/index.js
@@ -1,12 +1,11 @@
-import $ from 'jquery';
+import timeago from 'timeago.js';
 import MainView from '../main';
 
 export default class View extends MainView {
   mount() {
     super.mount();
 
-    $("time.timeago").timeago();
-
+    new timeago().render(document.querySelectorAll("time.timeago"));
     // Specific logic here
     console.log('HottestIndexView mounted');
   }
