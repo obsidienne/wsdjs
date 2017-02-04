6b201716ebd4fd298c1f5c2834400da30a79d2a7
claudio bernardes <bernardes.claudio@gmail.com>
2017-02-04 22:22:43 +0100

    display hot songs in order

--- apps/wsdjs_web/web/static/js/views/hottest/index.js
+++ apps/wsdjs_web/web/static/js/views/hottest/index.js
@@ -6,6 +6,7 @@ export default class View extends MainView {
     super.mount();
 
     new timeago().render(document.querySelectorAll("time.timeago"));
+    this._intlDate();
     // Specific logic here
     console.log('HottestIndexView mounted');
   }
@@ -17,4 +18,14 @@ export default class View extends MainView {
     console.log('HottestIndexView unmounted');
   }
 
+  _intlDate() {
+    var options = {year: "numeric", month: "long"};
+    var dateTimeFormat = new Intl.DateTimeFormat(undefined, options);
+
+    var elements = document.querySelectorAll("time:not(.timeago)");
+    for (let i = 0; i < elements.length; i++) {
+      let datetime = Date.parse(elements[i].getAttribute("datetime"))
+      elements[i].textContent = dateTimeFormat.format(datetime);
+    }
+  }
 }
--- apps/wsdjs_web/web/views/hottest_view.ex
+++ apps/wsdjs_web/web/views/hottest_view.ex
@@ -22,4 +22,19 @@ defmodule WsdjsWeb.HottestView do
                            to: account_path(conn, :show, song.account.id),
                            title: "#{song.account.name}")
   end
+
+  def group_song_by_month(songs) do
+    songs
+    |> Enum.group_by(fn (song) -> month_from_date(song.inserted_at) end)
+    |> Enum.sort_by(fn ({dt, _}) -> Date.to_erl(dt) end, &>=/2)
+  end
+
+  def month_from_date(date) do
+    {:ok, month} = Date.new(date.year, date.month, 1)
+    month
+  end
+
+  def sort_songs(songs) do
+    Enum.sort_by(songs, fn (dt) -> Date.to_erl(dt.inserted_at) end, &>=/2)
+  end
 end
