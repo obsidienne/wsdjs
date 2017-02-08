b82e336161aad8b8488705c080a9efa6fc6eca28
claudio bernardes <bernardes.claudio@gmail.com>
2017-02-08 18:43:18 +0100

    rights

--- apps/wsdjs_web/web/controllers/song_controller.ex
+++ apps/wsdjs_web/web/controllers/song_controller.ex
@@ -1,9 +1,28 @@
 defmodule WsdjsWeb.SongController do
   use WsdjsWeb.Web, :controller
 
+  plug :authenticate
+
   def show(conn, %{"id" => id}) do
     song = Wcsp.find_song!(id: id)
 
     render conn, "show.html", song: song
+  end
+
+
+  # check if the action page is authorized, then in the function according to the
+  # struct check if it's authorized
+  defp authenticate(conn, _opts) do
+    user = conn.assigns[:current_user]
+    action = conn.assigns[:action] || conn.private[:phoenix_action]
+
+    if Wscp.Policy.can?(user, action, Song) do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: session_path(conn, :new))
+      |> halt()
+    end
   end
 end
