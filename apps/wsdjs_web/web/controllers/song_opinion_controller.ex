6e9590446894970aaad10790598b6b81eff09007
claudio bernardes <bernardes.claudio@gmail.com>
2017-02-16 21:20:40 +0100

    Create link to update song opinion

---
+++ apps/wsdjs_web/web/controllers/song_opinion_controller.ex
@@ -0,0 +1,14 @@
+defmodule WsdjsWeb.SongOpinionController do
+  use WsdjsWeb.Web, :controller
+
+  plug :put_layout, false when action in [:create]
+
+  def create(conn, %{"kind" => kind, "song_id" => song_id}) do
+    user = conn.assigns[:current_user]
+
+    song = Wcsp.find_song!(user, id: song_id)
+
+    conn |>
+    render "show.html", song: song
+  end
+end
