64c729075bad8aa5bf5cd9e888ca26d2872bcc1c
Claudio Bernardes <bernardes.claudio@gmail.com>
2017-02-16 18:08:37 +0100

    test on comment length / presence

--- apps/wcsp/test/dj/song_comment_test.exs
+++ apps/wcsp/test/dj/song_comment_test.exs
@@ -14,4 +14,9 @@ defmodule Wcsp.SongCommentTest do
     comment = SongComment.changeset(%SongComment{}, params)
     assert {:error, %{errors: [user: {"does not exist", _}]}} = Repo.insert(comment)
   end
+
+  test "song comment length > 0" do
+    comment = SongComment.changeset(%SongComment{}, %{text: ""})
+    assert {:error, %{errors: [text: {"can't be blank", _}]}} = Repo.insert(comment)
+  end
 end
