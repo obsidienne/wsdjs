edeeabc410b1ce8e34030262d89d60dbde070b7b
claudio bernardes <bernardes.claudio@gmail.com>
2017-02-21 09:12:11 +0100

    remove commented code

--- apps/wsdjs_web/web/static/js/views/shared/ujs.js
+++ apps/wsdjs_web/web/static/js/views/shared/ujs.js
@@ -32,19 +32,3 @@ export default class glSearch {
     });
   }
 }
-
-/*
-var timer;
-
-document.getElementById("glsearch").addEventListener('keyup', function() {
-  clearTimeout(timer);  //clear any running timeout on key up
-  timer = setTimeout(function() { //then give it a second to see if the user is finished
-    var query = document.getElementById("glsearch").value;
-
-    $.get('/search', { q: query }).done(function( data ) {
-      document.querySelector(".glsearch__results").innerHTML = data;
-      new timeago().render(document.querySelectorAll(".glsearch__results time.timeago"));
-    });
-  }, 500);
-});
-*/
