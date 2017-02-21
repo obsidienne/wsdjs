4ee3a4b9796e0171fca4dbe16328ba2cdf07c9f3
claudio bernardes <bernardes.claudio@gmail.com>
2017-02-22 00:55:19 +0100

    simplify everything

--- apps/wsdjs_web/web/static/js/views/hottest/index.js
+++ apps/wsdjs_web/web/static/js/views/hottest/index.js
@@ -7,6 +7,7 @@ export default class View extends MainView {
 
     new timeago().render(document.querySelectorAll("time.timeago"));
     this._intlDate();
+    this._set_opinion();
 
     // Specific logic here
     console.log('HottestIndexView mounted');
@@ -30,5 +31,37 @@ export default class View extends MainView {
     }
   }
 
+  _set_opinion() {
+    document.addEventListener("click",function(e) {
+      if (e.target && e.target.matches(".song-opinion")) {
+        e.preventDefault();
 
+        var self = e.target;
+        var method = e.target.dataset.method;
+        var url = e.target.href;
+        var csrf = document.querySelector("[name=csrf-token]").getAttribute("content");
+        var token = document.querySelector("[name=channel_token]").getAttribute("content");
+
+        var request = new XMLHttpRequest();
+        request.open(method, url, true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.setRequestHeader('x-csrf-token', csrf);
+        request.setRequestHeader('Authorization', "Bearer " + token);
+
+        request.onload = function() {
+          if (this.status >= 200 && this.status < 400) {
+            // Success!
+            self.parentNode.innerHTML = this.response;
+          } else {
+            console.error("Error");
+            // We reached our target server, but it returned an error
+          }
+        };
+
+        // There was a connection error of some sort
+        request.onerror = function() { console.error("Error"); };
+        request.send();
+	     }
+    });
+  }
 }
