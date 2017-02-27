9314ce214bed54aedede75fce970277e66aeb5c1
claudio bernardes <bernardes.claudio@gmail.com>
2017-02-27 17:39:01 +0100

    clean hottest view

--- apps/wsdjs_web/web/views/hottest_view.ex
+++ apps/wsdjs_web/web/views/hottest_view.ex
@@ -1,21 +1,24 @@
 defmodule WsdjsWeb.HottestView do
   use WsdjsWeb.Web, :view
 
+  @base_url "http://res.cloudinary.com/don2kwaju/image/upload/"
+  @small_format "ar_1:1,c_fill,g_auto/w_auto:100:250/dpr_auto/f_auto,q_auto/"
+  @missing_song_art "v1/wsdjs/missing_cover.jpg"
+
   def href_song_art(%{cld_id: cld_id, version: version}) when is_binary(cld_id) do
-    "http://res.cloudinary.com/don2kwaju/image/upload/ar_1:1,c_fill,g_auto/w_auto:100:250/dpr_auto/f_auto,q_auto/v#{version}/#{cld_id}.jpg"
+    @base_url <> @small_format <> "v#{version}/" <> "#{cld_id}.jpg"
   end
-  def href_song_art(_), do: "http://res.cloudinary.com/don2kwaju/image/upload/ar_1:1,c_fill,g_auto/w_auto:100:250/dpr_auto/f_auto,q_auto/v1/wsdjs/missing_cover.jpg"
+  def href_song_art(_), do: @base_url <> @small_format <> @missing_song_art
 
   def song_art_alt(song) do
     "#{song.artist} - #{song.title} song art"
   end
 
   def proposed_by_display_name(%{name: name, djname: djname}) when is_binary(djname) do
-    djname
+    "#{name} (#{djname})"
   end
-  def proposed_by_display_name(%{name: name, djname: djname}) do
-    name
-  end
+  def proposed_by_display_name(%{name: name}), do: name
+
 
   def proposed_by_link(conn, song = %Wcsp.Song{}) do
     Phoenix.HTML.Link.link(proposed_by_display_name(song.user),
