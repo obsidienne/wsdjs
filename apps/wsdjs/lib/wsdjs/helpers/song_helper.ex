9ce5ef75543977d99c303f66cc1e5269808f967d
claudio bernardes <bernardes.claudio@gmail.com>
2017-03-16 23:53:13 +0100

    Song styling

--- apps/wsdjs/lib/wsdjs/helpers/song_helper.ex
+++ apps/wsdjs/lib/wsdjs/helpers/song_helper.ex
@@ -1,5 +1,6 @@
 defmodule Wsdjs.SongHelper do
   @base_url "https://res.cloudinary.com/don2kwaju/image/upload/w_auto/c_scale/"
+  @base_url_blured "https://res.cloudinary.com/don2kwaju/image/upload/w_auto/c_scale/e_blur:800/o_30/"
   @missing_song_art "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8//PIfwAJeAO9U/c7OgAAAABJRU5ErkJggg=="
 
   def song_art_href(%Wcsp.AlbumArt{cld_id: cld_id, version: version}) when is_binary(cld_id) do
@@ -7,7 +8,14 @@ defmodule Wsdjs.SongHelper do
   end
   def song_art_href(nil), do: @missing_song_art
 
+  def song_art_href_blured(%Wcsp.AlbumArt{cld_id: cld_id, version: version}) when is_binary(cld_id) do
+    @base_url_blured <> "v#{version}/" <> "#{cld_id}.jpg"
+  end
+  def song_art_href_blured(nil), do: @missing_song_art
+
+
   def song_art_href_default(), do: @missing_song_art
+
 
   def song_art_alt(%Wcsp.Song{artist: artist, title: title}), do: "#{artist} - #{title} song art"
 
