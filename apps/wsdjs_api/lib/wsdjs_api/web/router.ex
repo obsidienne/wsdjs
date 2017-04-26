270c429ac7e5080fab25c63b08b505022ce2e232
claudio bernardes <bernardes.claudio@gmail.com>
2017-04-26 08:43:23 +0200

    duplicate files for api auth

--- apps/wsdjs_api/lib/wsdjs_api/web/router.ex
+++ apps/wsdjs_api/lib/wsdjs_api/web/router.ex
@@ -3,6 +3,7 @@ defmodule WsdjsApi.Web.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug Wsdjs.VerifyHeader
   end
 
   scope "/api", WsdjsApi.Web do
@@ -11,5 +12,9 @@ defmodule WsdjsApi.Web.Router do
     resources "/ranks", RankController, except: [:index, :show, :new, :edit]
     resources "/opinions", OpinionController, except: [:new, :edit]
 
+  end
+
+  pipeline :api_auth do
+    plug Wsdjs.EnsureAuthenticated, handler_fn: :api_call
   end
 end
