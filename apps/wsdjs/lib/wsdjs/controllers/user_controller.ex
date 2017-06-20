7940a8a6228b832eca6c6fd3bb6139bcb1af511e
Phets <pphommar@gmail.com>
2017-06-20 13:42:59 +0200

    Edit page info

--- apps/wsdjs/lib/wsdjs/controllers/user_controller.ex
+++ apps/wsdjs/lib/wsdjs/controllers/user_controller.ex
@@ -17,21 +17,30 @@ defmodule Wsdjs.UserController do
     render conn, "show.html", user: user, songs: songs
   end
 
+  @countries ["EN", "FR", "US"]
+
   def edit(conn, %{"id" => id}) do
     current_user = conn.assigns[:current_user]
     user = Wcsp.Accounts.get_user!(id)
     changeset = Wcsp.Accounts.User.changeset(%Wcsp.Accounts.User{})
-    render conn, "edit.html", user: user, changeset: changeset
+    countries = @countries
+    render conn, "edit.html", user: user, changeset: changeset, countries: countries
   end
 
-  def update(conn, %{"id" => id}) do
+  def update(conn, %{"id" => id, "user" => params}) do
     current_user = conn.assigns[:current_user]
-    
-    # top = Wcsp.Trendings.get_top(current_user, id)
-    # Wcsp.Trendings.next_step(current_user, top)
-    # top = Wcsp.Trendings.get_top(current_user, id)
-
-    redirect(conn, to: user_path(conn, :show, current_user))
+    changeset = Wcsp.Accounts.User.changeset(current_user, params)
+    case Wcsp.Repo.update(changeset) do
+      {:ok, user} -> 
+        IO.inspect user
+        conn        
+        |> put_flash(:info, "Profile updated")
+        |> redirect(to: user_path(conn, :show, current_user))
+      _ -> 
+        conn
+        |> put_flash(:error, "Something went wrong !")
+        |> redirect(to: user_path(conn, :edit, current_user))
+    end
   end
 
 end
