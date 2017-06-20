9bdf03911f14858f0563e43cf70bd9cf5d6a803d
Claudio Bernardes <bernardes.claudio@gmail.com>
2017-06-20 03:32:22 +0200

    Force ssl for heroku deploiement

--- apps/wsdjs/config/prod.exs
+++ apps/wsdjs/config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :wsdjs, Wsdjs.Endpoint,
   on_init: {Wsdjs.Endpoint, :load_from_system_env, []},
-  url: [host: "wcsp.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "wcsp.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
