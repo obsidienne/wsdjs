e523b5062f30f4c958b56b724c3a71cb4ada3096
claudio bernardes <bernardes.claudio@gmail.com>
2017-06-22 22:53:38 +0200

    database prod name when launch locally

--- apps/wsdjs/config/prod.exs
+++ apps/wsdjs/config/prod.exs
@@ -2,5 +2,5 @@ use Mix.Config
 
 config :wsdjs, Wsdjs.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL") || "postgres://postgres:postgres@localhost:5432/rwp",
+  url: System.get_env("DATABASE_URL") || "postgres://postgres:postgres@localhost:5432/wsdjs",
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
