48abef5a99f490ea7bb1e7e3f7d1e3f93d3c9368
claudio bernardes <bernardes.claudio@gmail.com>
2017-06-22 09:42:10 +0200

    add credo

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Wsdjs.Umbrella.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [{:credo, "~> 0.8", only: [:dev, :test], runtime: false}]
   end
 
   defp aliases do
