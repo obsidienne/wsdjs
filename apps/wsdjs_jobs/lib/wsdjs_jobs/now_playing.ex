6db97c4e8fd6126bb6b4559ebdbb6000e7be3a20
claudio bernardes <bernardes.claudio@gmail.com>
2017-06-22 22:50:07 +0200

    now_playing refactoring

--- apps/wsdjs_jobs/lib/wsdjs_jobs/now_playing.ex
+++ apps/wsdjs_jobs/lib/wsdjs_jobs/now_playing.ex
@@ -27,78 +27,18 @@ defmodule Wsdjs.Jobs.NowPlaying do
     {:reply, Enum.reverse(list), queue}
   end
 
-  defp metadata(body) do
-    text_without_tags = HtmlSanitizeEx.strip_tags(body)
-    list = String.split(text_without_tags, "," , parts: 7, trim: true)
-    Enum.at(list, 6)
-  end
-
-  defp push_song(seven_response_html, queue) do
-    artist = ""
-    title = ""
-    if (:queue.len(queue) > 0) do
-      last_song_in_queue = :queue.get_r(queue)
-      artist = last_song_in_queue[:artist]
-      title = last_song_in_queue[:title]
-    end
-
-    current_metadata = "#{artist} - #{title}"
-    new_metadata = metadata(seven_response_html)
-    if current_metadata != new_metadata do
-      [artist, title] = String.split(new_metadata, " - ", parts: 2, trim: true)
-
-      song = %{
-        artist: artist,
-        title: title,
-        ts: :os.system_time(:seconds)
-      }
-
-      song_in_base = Wsdjs.Musics.search_artist_title(new_metadata)
-      if song_in_base != nil do
-        song = Map.put(song, :image_uri, Wsdjs.Web.SongHelper.song_art_href(song_in_base.art))
-        song = Map.put(song, :suggested_by, song_in_base.user.name)
-        song = Map.put(song, :suggested_by_path, "/users/#{song_in_base.user.id}")
-        song = Map.put(song, :path, "/songs/#{song_in_base.id}")
-        song = Map.put(song, :suggested_ts, Timex.to_unix(song_in_base.inserted_at))
-        tags = []
-        if length(song_in_base.tops) > 0 do
-          top_head = Enum.at(song_in_base.tops, 0)
-          if length(top_head.ranks) > 0 do
-            rank_head = Enum.at(top_head.ranks , 0)
-
-            if (rank_head.position < 10) do
-              top = "Top " <> Timex.format!(top_head.due_date, "%b%y", :strftime)
-              tags = tags ++ [top]
-            end
-          end
-        end
-        song = Map.put(song, :tags, tags)
-        PubSub.broadcast Wsdjs.Web.PubSub, "notifications:now_playing", :new_played_song
-      end
-
-      if (:queue.len(queue) > 9) do
-        queue = :queue.drop(queue)
-      end
-      queue = :queue.in(song, queue)
-
-      IO.inspect song
-    end
-
-    queue
-  end
-
   @seven_url "http://37.58.75.166:8384/7.html"
   def handle_info(:work, queue) do
     case HTTPoison.get(@seven_url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        queue = push_song(body, queue)
+        queue = parse_streamed_song(body, queue)
         schedule_work() # Reschedule once more
         {:noreply, queue}
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         Logger.debug "Not found :("
         {:noreply, queue}
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+        Logger.error reason
         {:noreply, queue}
     end
   end
@@ -107,4 +47,65 @@ defmodule Wsdjs.Jobs.NowPlaying do
     Process.send_after(self(), :work, 2 * 1 * 1 * 1000) # In 2 seconds
   end
 
+  defp parse_streamed_song(body, queue) do
+    last_queued = last_song_queued(:queue.peek_r(queue))
+
+    body
+    |> HtmlSanitizeEx.strip_tags()
+    |> String.split("," , parts: 7, trim: true)
+    |> Enum.at(6)
+    |> push_song(last_queued, queue)
+  end
+
+  defp last_song_queued(:empty), do: ""
+  defp last_song_queued({:value, song}), do: "#{song[:artist]} - #{song[:title]}"
+
+  defp push_song(song, song, queue), do: queue
+  defp push_song(streamed_song, _last_song, queue) do
+    [artist, title] = String.split(streamed_song, " - ", parts: 2, trim: true)
+
+    queue = %{artist: artist, title: title, ts: :os.system_time(:seconds)}
+            |> filled_from_db(streamed_song)
+            |> :queue.in(queue)
+
+    PubSub.broadcast Wsdjs.Web.PubSub, "notifications:now_playing", :new_played_song
+
+    if :queue.len(queue) > 9 do
+      :queue.drop(queue)
+    else
+      queue
+    end
+  end
+
+  defp filled_from_db(song, streamed_song) do
+    song_in_base = Wsdjs.Musics.search_artist_title(streamed_song)
+
+    if song_in_base != nil do
+      song
+      |> Map.put(:artist, song_in_base.artist)
+      |> Map.put(:title, song_in_base.title)
+      |> Map.put(:image_uri, Wsdjs.Web.SongHelper.song_art_href(song_in_base.art))
+      |> Map.put(:suggested_by, song_in_base.user.name)
+      |> Map.put(:suggested_by_path, "/users/#{song_in_base.user.id}")
+      |> Map.put(:path, "/songs/#{song_in_base.id}")
+      |> Map.put(:suggested_ts, Timex.to_unix(song_in_base.inserted_at))
+      |> Map.put(:tags, tags_for_song(song_in_base))
+    else
+      song
+    end
+  end
+
+  defp tags_for_song(song_in_base) do
+    with {:ok, top_head} <- Enum.fetch(song_in_base.tops, 0),
+         {:ok, rank_head} <- Enum.fetch(top_head.ranks, 0),
+         {:ok, _} <- song_position(rank_head.position)
+    do
+      ["Top " <> Timex.format!(top_head.due_date, "%b%y", :strftime)]
+    else
+      []
+    end
+  end
+
+  defp song_position(pos) when pos <= 10, do: {:ok, pos}
+  defp song_position(pos) when pos > 10, do: {:ko, pos}
 end
