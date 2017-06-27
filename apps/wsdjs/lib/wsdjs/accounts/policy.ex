b58ff1b2513e69eb495e91486cd4c4f3e6c41d96
claudio bernardes <bernardes.claudio@gmail.com>
2017-06-27 22:42:06 +0200

    policies modifications

--- apps/wsdjs/lib/wsdjs/accounts/policy.ex
+++ apps/wsdjs/lib/wsdjs/accounts/policy.ex
@@ -1,17 +1,14 @@
-defmodule Wsdjs.Musics.Policy do
+defmodule Wsdjs.Accounts.Policy do
   @moduledoc """
   The Policies for the Dj system.
   """
   alias Wsdjs.Accounts.User
-  alias Wsdjs.Musics.Song
 
   @doc """
   A connected user can create a song
   """
-  def can?(:create_song, %User{}), do: true
-
-  def can?(:edit_user, %User{admin: true}, %Song{}), do: true
-  def can?(:edit_user, %User{id: id}, %Song{user_id: id}), do: true
+  def can?(:edit_user, %User{admin: true}, _), do: true
+  def can?(:edit_user, %User{id: id}, %User{id: id}), do: true
 
 
   @doc """
