246adbfd3869df23c5100d5de9003e0ad168f059
Phets <pphommar@gmail.com>
2017-07-04 10:23:46 +0200

    Date for text version of top

--- apps/wsdjs_web/lib/wsdjs_web/controllers/top_text_controller.ex
+++ apps/wsdjs_web/lib/wsdjs_web/controllers/top_text_controller.ex
@@ -1,6 +1,9 @@
 defmodule Wsdjs.Web.TopTextController do
   use Wsdjs.Web, :controller
   
+  require Timex
+  require Date
+
   def index(conn, %{"top_id" => id}) do
     current_user = conn.assigns[:current_user]
 
@@ -9,8 +12,11 @@ defmodule Wsdjs.Web.TopTextController do
     ranks = Enum.take(top.ranks, 20)
     response = Enum.map(ranks, fn (rank) ->
       song = rank.song      
-      points = rank.likes + rank.bonus + rank.votes            
-      ["#{rank.position}. ", song.artist, " ", song.title, " (", song.genre, ") - ", Integer.to_string(points) ," pts suggested by ", song.user.name, " on DATE\n"]
+      points = rank.likes + rank.bonus + rank.votes       
+      datetime = song.inserted_at
+      year = String.slice(Integer.to_string(datetime.year), 2..4)
+      date = Timex.month_shortname(datetime.month) <> " " <> year
+      ["#{rank.position}. ", song.artist, " ", song.title, " (", song.genre, ") - ", Integer.to_string(points) ," pts suggested by ", song.user.name, " on ", date, "\n"]
     end)
 
     text conn, response    
