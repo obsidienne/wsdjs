138ce448f0e2137eeb63639130e921a084b57c3a
claudio bernardes <bernardes.claudio@gmail.com>
2017-07-07 20:45:44 +0200

    Renaming

--- apps/wsdjs_web/lib/wsdjs_web/helpers/cloudinary_helper.ex
+++ apps/wsdjs_web/lib/wsdjs_web/helpers/cloudinary_helper.ex
@@ -1,28 +1,29 @@
 defmodule Wsdjs.Web.CloudinaryHelper do
-  @base_url "//res.cloudinary.com/don2kwaju/image/upload/w_auto/c_crop,g_custom/"
-  @base_url_blured "//res.cloudinary.com/don2kwaju/image/upload/w_auto,h_350,c_crop,g_custom/e_blur:300/o_30/"
-  @missing_song_art "//res.cloudinary.com/don2kwaju/image/upload/v1449164620/wsdjs/missing_cover.jpg"
+  alias Wsdjs.Musics.Art
+  alias Wsdjs.Accounts.Avatar
 
-  def art_url(%Wsdjs.Musics.Art{cld_id: cld_id, version: version}) when is_binary(cld_id) do
-    @base_url <> "v#{version}/" <> "#{cld_id}.jpg"
+  @art_root_url "//res.cloudinary.com/don2kwaju/image/upload/w_auto/c_crop,g_custom/"
+  @art_blured_root_url "//res.cloudinary.com/don2kwaju/image/upload/w_auto,h_350,c_crop,g_custom/e_blur:300/o_30/"
+  @art_missing_url "//res.cloudinary.com/don2kwaju/image/upload/v1449164620/wsdjs/missing_cover.jpg"
+
+  def art_url(%Art{cld_id: cld_id, version: version}) when is_binary(cld_id) do
+    @art_root_url <> "v#{version}/" <> "#{cld_id}.jpg"
   end
-  def art_url(_), do: @missing_song_art
-  def art_url(), do: @missing_song_art
+  def art_url(_ \\ nil), do: @art_missing_url
 
-  def art_url_blured(%Wsdjs.Musics.Art{cld_id: cld_id, version: version}) when is_binary(cld_id) do
-    @base_url_blured <> "v#{version}/" <> "#{cld_id}.jpg"
+  def art_url_blured(%Art{cld_id: cld_id, version: version}) when is_binary(cld_id) do
+    @art_blured_root_url <> "v#{version}/" <> "#{cld_id}.jpg"
   end
-  def art_url_blured(nil), do: @missing_song_art
+  def art_url_blured(nil), do: @art_missing_url
 
 
 
 
-  @avatar_base_url "//res.cloudinary.com/don2kwaju/image/upload/w_auto/c_crop,g_custom/"
-  @missing_avatar "//res.cloudinary.com/don2kwaju/image/upload/v1450094305/wsdjs/missing_avatar.jpg"
+  @avatar_root_url "//res.cloudinary.com/don2kwaju/image/upload/w_auto/c_crop,g_custom/"
+  @avatar_missing_url "//res.cloudinary.com/don2kwaju/image/upload/v1450094305/wsdjs/avatar_missing_url.jpg"
 
-  def avatar_url(%Wsdjs.Accounts.Avatar{cld_id: cld_id, version: version}) when is_binary(cld_id) do
-    @avatar_base_url <> "v#{version}/" <> "#{cld_id}.jpg"
+  def avatar_url(%Avatar{cld_id: cld_id, version: version}) when is_binary(cld_id) do
+    @avatar_root_url <> "v#{version}/" <> "#{cld_id}.jpg"
   end
-  def avatar_url(_), do: @missing_avatar
-  def avatar_url(), do: @missing_avatar
+  def avatar_url(_ \\ nil), do: @avatar_missing_url
 end
