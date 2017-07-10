e86e8416fc8f5cb8cc1ff141a1c960e94f7fc111
Claudio Bernardes <bernardes.claudio@gmail.com>
2017-07-11 01:56:55 +0200

    refactor radio player and add hook to click on song when data-video-id is set (youtube video)

--- apps/wsdjs_web/assets/js/components/radio.js
+++ apps/wsdjs_web/assets/js/components/radio.js
@@ -7,71 +7,70 @@ export default class Radio {
     this.radio = new Audio("http://37.58.75.166:8384/stream?icy=http");
 
     // Now that you are connected, you can join channels with a topic:
-    let channel = socket.channel("notifications:now_playing", {})
-    channel.join()
+    this.channel = socket.channel("notifications:now_playing", {})
+    this.channel.join()
       .receive("ok", resp => { console.log("Joined successfully", resp) })
       .receive("error", resp => { console.log("Unable to join", resp) })
 
-    channel.on("new_played_song", payload => {
-      this.refresh(payload)
+    this.channel.on("new_played_song", payload => {
+      this.refresh_radio(payload)
     });
 
     document.addEventListener("click", function(e) {
-      if (e.target && e.target.matches(".toggle-player")){
-        if (!self.radio.paused) { // play to pause
-          self.radio.pause();
-          e.target.classList.add("icon-play");
-          e.target.classList.remove("icon-pause");
-          document.querySelector(".miniplayer-art img").src = '//res.cloudinary.com/don2kwaju/image/upload/wsdjs/radiowcs_square.jpg';
-          var current_play_title = document.querySelector(".miniplayer-info h6:first-child");
-          var current_play_artist = document.querySelector(".miniplayer-info h6:nth-child(2)");
-          var current_play_suggestor = document.querySelector(".miniplayer-info h6:last-child");
-          current_play_title.innerHTML = "";
-          current_play_artist.innerHTML = "";
-          current_play_suggestor.innerHTML = "Play the radio";
-
-        } else { // pause to play
-          channel.push("played_song_list")
-          self.radio.play();
-          e.target.classList.add("icon-pause");
-          e.target.classList.remove("icon-play");
+      if (e.target && e.target.matches(".toggle-player")) {
+        if (self.radio.paused) {
+          self.play_radio(e.target)
+        } else {
+          self.pause_radio(e.target)
         }
       }
-    });
+
+      if (e.target && e.target.matches("[data-video-id]")) {
+        e.preventDefault();
+        e.stopPropagation();
+        self.youtube(e.target.dataset.videoId);
+      }
+    }
+    );
     console.log('Radio component mounted');
   }
 
-  refresh(payload) {
-    var player = document.querySelector("#radio-container");
+  youtube(video_id) {
+    console.log(video_id);
+  }
 
+  pause_radio(el) {
+    this.radio.pause();
+    el.classList.add("icon-play");
+    el.classList.remove("icon-pause");
+    document.querySelector(".miniplayer-art img").src = '//res.cloudinary.com/don2kwaju/image/upload/wsdjs/radiowcs_square.jpg';
+    document.querySelector(".miniplayer-info h6:first-child").innerHTML = "";
+    document.querySelector(".miniplayer-info h6:nth-child(2)").innerHTML = "";
+    document.querySelector(".miniplayer-info h6:last-child").innerHTML = "Play the radio";
+  }
 
+  play_radio(el) {
+    this.channel.push("played_song_list")
+    this.radio.play();
+    el.classList.add("icon-pause");
+    el.classList.remove("icon-play");
+  }
+
+  refresh_radio(payload) {
     var data = JSON.parse(payload.data);
 
+    document.querySelector(".miniplayer-art img").setAttribute("src", data[0].image_uri);;
+    document.querySelector(".miniplayer-info").setAttribute("href", data[0].path);;
+    document.querySelector(".miniplayer-info h6:first-child").innerHTML = data[0].title;;
+    document.querySelector(".miniplayer-info h6:nth-child(2)").innerHTML = data[0].artist;;
+    document.querySelector(".miniplayer-info h6:last-child").innerHTML = `<span class="suggested_by">suggested by ${data[0].suggested_by}</span>`;;
+
     var playing = "";
-    var max_it = 5;
-    if (document.documentElement.clientHeight <= 480) {
-      max_it = 2
-    }
-    for (let i = 1; i < data.length && i < max_it; i++) {
+    for (let i = 1; i < data.length && i < 5; i++) {
       playing += `<li class="played-song tippy" data-position="top-end" data-size="small" title="${data[i].artist} - ${data[i].title}<br/><span class='small'>Suggested by ${data[i].suggested_by}</span>"><a href="${data[i].path}"><img height="50" width="50" src="${data[i].image_uri}" /></a></li>`;
     }
+    document.querySelector("#radio-container").innerHTML = playing;
 
-    var art = document.querySelector(".miniplayer-art img");
-    art.setAttribute("src", data[0].image_uri);
-
-    var current_play = document.querySelector(".miniplayer-info");
-    var current_play_title = document.querySelector(".miniplayer-info h6:first-child");
-    var current_play_artist = document.querySelector(".miniplayer-info h6:nth-child(2)");
-    var current_play_suggestor = document.querySelector(".miniplayer-info h6:last-child");
-
-    current_play.setAttribute("href", data[0].path);
-    current_play_title.innerHTML = data[0].title;
-    current_play_artist.innerHTML = data[0].artist;
-    current_play_suggestor.innerHTML = `<span class="suggested_by">suggested by ${data[0].suggested_by}</span>`;
-
-    console.log(playing);
-    player.innerHTML = playing;
     new Tippy('.tippy');
-
   }
 }
