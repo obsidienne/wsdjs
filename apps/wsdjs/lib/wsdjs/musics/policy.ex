29e9353fe9f36e87d9c4d1e747489ef1e0b5c82c
claudio bernardes <bernardes.claudio@gmail.com>
2017-07-10 09:16:14 +0200

    use policy in wsdjs app instead of condition in view helper

--- apps/wsdjs/lib/wsdjs/musics/policy.ex
+++ apps/wsdjs/lib/wsdjs/musics/policy.ex
@@ -5,13 +5,18 @@ defmodule Wsdjs.Musics.Policy do
   alias Wsdjs.Accounts.User
   alias Wsdjs.Musics.Song
 
-  @doc """
-  A connected user can create a song
-  """
+  @doc "A connected user can create a song."
   def can?(:create_song, %User{}), do: true
 
-  def can?(:edit_user, %User{admin: true}, %Song{}), do: true
-  def can?(:edit_user, %User{id: id}, %Song{user_id: id}), do: true
+  @doc "Admin can edit a song."
+  def can?(:edit_song, %User{admin: true}, %Song{}), do: true
+  @doc "A connected user can edit his own song."
+  def can?(:edit_song, %User{id: id}, %Song{user_id: id}), do: true
+
+  @doc "Admin can delete a song."
+  def can?(:delete_song, %User{admin: true}, %Song{}), do: true
+  @doc "A connected user can delete his own song."
+  def can?(:delete_song, %User{id: id}, %Song{user_id: id}), do: true
 
 
   @doc """
--- apps/wsdjs_web/lib/wsdjs_web/helpers/song_helper.ex
+++ apps/wsdjs_web/lib/wsdjs_web/helpers/song_helper.ex
@@ -2,8 +2,5 @@ defmodule Wsdjs.Web.SongHelper do
   def song_art_alt(%Wsdjs.Musics.Song{artist: artist, title: title}), do: "#{artist} - #{title} song art"
 
   def song_full_title(%Wsdjs.Musics.Song{artist: artist, title: title}), do: "#{artist}\u000A#{title}"
-
-  def can_edit_song?(current_user, song) do
-    song.user.id == current_user.id || current_user.admin 
-  end
 end
+ 
