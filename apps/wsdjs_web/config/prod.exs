e45d6b1871535648af96cc7995dac5563ce79d96
claudio bernardes <bernardes.claudio@gmail.com>
2017-07-11 18:39:09 +0200

    Remove ref to plyr, update mix.lock and add port to URL in prod config

--- apps/wsdjs_web/config/prod.exs
+++ apps/wsdjs_web/config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :wsdjs_web, Wsdjs.Web.Endpoint,
   on_init: {Wsdjs.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "wsdjs.cleverapps.io"],
+  url: [host: "wsdjs.cleverapps.io", port: 8080],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
