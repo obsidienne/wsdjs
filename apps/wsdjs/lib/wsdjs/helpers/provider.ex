edfb4aaa99a25de1459229a210a725ffbd515601
claudio bernardes <bernardes.claudio@gmail.com>
2017-07-12 02:36:12 +0200

    testing provider

--- apps/wsdjs/lib/wsdjs/helpers/provider.ex
+++ apps/wsdjs/lib/wsdjs/helpers/provider.ex
@@ -7,6 +7,15 @@ defmodule Wsdjs.Helpers.Provider do
       {~r/youtu(?:\.be|be\.com)\/(?:.*v(?:\/|=)|(?:.*\/)?)([\w'-]+)/i, :youtube}
   ]
 
+  @doc ~S"""
+  Should match the following youtube URLs
+
+  ## Examples
+      iex> extract("http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo")
+      "1p3vcRhsYGo"
+      iex> extract(nil)
+      nil
+  """
   def extract(url) when is_nil(url), do: nil
 
   def extract(url) do
@@ -17,43 +26,10 @@ defmodule Wsdjs.Helpers.Provider do
     Kernel.apply(Wsdjs.Helpers.Provider, func, [re, url])
   end
 
+  @doc false
   def fn_unknown(_re, _url), do: nil
 
-  @doc ~S"""
-    Should match the following youtube URLs
-
-    ## Examples
-
-      iex> re = ~r/youtu(?:\.be|be\.com)\/(?:.*v(?:\/|=)|(?:.*\/)?)([\w'-]+)/i
-      iex> youtube(re, "http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo")
-      "1p3vcRhsYGo"
-      iex> youtube(re, "http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel")
-      "cKZDdG9FTKY"
-      iex> youtube(re, "http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub")
-      "yZ-K7nCVnBI"
-      iex> youtube(re, "http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I")
-      "NRHVzbJVx8I"
-      iex> youtube(re, "http://www.youtube.com/user/SilkRoadTheatre#p/a/u/2/6dwqZw0j_jY")
-      "6dwqZw0j_jY"
-      iex> youtube(re, "http://youtu.be/6dwqZw0j_jY")
-      "6dwqZw0j_jY"
-      iex> youtube(re, "http://www.youtube.com/watch?v=6dwqZw0j_jY&feature=youtu.be")
-      "6dwqZw0j_jY"
-      iex> youtube(re, "http://youtu.be/afa-5HQHiAs")
-      "afa-5HQHiAs"
-      iex> youtube(re, "http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo?rel=0")
-      "1p3vcRhsYGo"
-      iex> youtube(re, "http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel")
-      "cKZDdG9FTKY"
-      iex> youtube(re, "http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub")
-      "yZ-K7nCVnBI"
-      iex> youtube(re, "http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I")
-      "NRHVzbJVx8I"
-      iex> youtube(re, "http://www.youtube.com/embed/nas1rJpm7wY?rel=0")
-      "nas1rJpm7wY"
-      iex> youtube(re, "http://www.youtube.com/watch?v=peFZbP64dsU")
-      "peFZbP64dsU"
-  """
+  @doc false
   def youtube(re, url) do
     [_, video_id] = Regex.run(re, url)
     video_id
--- apps/wsdjs/test/wsdjs/helpers/provider_test.exs
+++ apps/wsdjs/test/wsdjs/helpers/provider_test.exs
@@ -3,5 +3,26 @@ defmodule Wsdjs.ProviderTest do
 
   use Wsdjs.DataCase
   doctest Wsdjs.Helpers.Provider, import: true
+
+  @youtube_urls [
+      {"http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo", "1p3vcRhsYGo"},
+      {"http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel", "cKZDdG9FTKY"},
+      {"http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub", "yZ-K7nCVnBI"},
+      {"http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I", "NRHVzbJVx8I"},
+      {"http://www.youtube.com/user/SilkRoadTheatre#p/a/u/2/6dwqZw0j_jY", "6dwqZw0j_jY"},
+      {"http://youtu.be/6dwqZw0j_jY", "6dwqZw0j_jY"},
+      {"http://www.youtube.com/watch?v=6dwqZw0j_jY&feature=youtu.be", "6dwqZw0j_jY"},
+      {"http://youtu.be/afa-5HQHiAs", "afa-5HQHiAs"},
+      {"http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo?rel=0", "1p3vcRhsYGo"},
+      {"http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel", "cKZDdG9FTKY"},
+      {"http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub", "yZ-K7nCVnBI"},
+      {"http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I", "NRHVzbJVx8I"},
+      {"http://www.youtube.com/embed/nas1rJpm7wY?rel=0", "nas1rJpm7wY"},
+      {"http://www.youtube.com/watch?v=peFZbP64dsU", "peFZbP64dsU"}
+  ]
+
+  test "youtube urls" do
+    Enum.each(@youtube_urls, fn {url, video_id} -> assert Wsdjs.Helpers.Provider.extract(url) == video_id end)
+  end
 end
 
