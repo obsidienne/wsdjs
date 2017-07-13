f0f96db4f7897eab0c65e979d19b44a14c3f5c69
claudio bernardes <bernardes.claudio@gmail.com>
2017-07-13 22:09:41 +0200

    More cleaning

--- apps/wsdjs_web/lib/wsdjs_web/controllers/user_controller.ex
+++ apps/wsdjs_web/lib/wsdjs_web/controllers/user_controller.ex
@@ -1,20 +1,21 @@
 defmodule Wsdjs.Web.UserController do
   use Wsdjs.Web, :controller
 
-  def index(conn, _params) do
-    users = Wsdjs.Accounts.list_users()
+  alias Wsdjs.Accounts
+  alias Wsdjs.Musics
 
+  @doc false
+  def index(conn, _params) do
+    users = Accounts.list_users()
     render conn, "index.html", users: users
   end
 
-  @doc """
-  No authZ needed, data is scoped by current_user
-  """
+  @doc false
   def show(conn, %{"id" => user_id}) do
     current_user = conn.assigns[:current_user]
-    user = Wsdjs.Accounts.get_user!(user_id)
+    user = Accounts.get_user!(user_id)
     
-    page = Wsdjs.Musics.paginate_songs_user(current_user, user_id)
+    page = Musics.paginate_songs_user(current_user, user_id)
 
     conn
     |> put_resp_header("total-pages", Integer.to_string(page.total_pages))
@@ -22,22 +23,19 @@ defmodule Wsdjs.Web.UserController do
     |> render("show.html", user: user, songs: page.entries, page_number: page.page_number, total_pages: page.total_pages, total_songs: page.total_entries)
   end
 
-  @doc """
-  Authz needed
-  """
+  @doc false
   def edit(conn, %{"id" => id}) do
-    user = Wsdjs.Accounts.get_user!(id)
-    changeset = Wsdjs.Accounts.change_user(user)
+    user = Accounts.get_user!(id)
+    changeset = Accounts.change_user(user)
     render conn, "edit.html", user: user, changeset: changeset
   end
 
-  @doc """
-  Authz needed
-  """
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Wsdjs.Accounts.get_user!(id)
 
-    case Wsdjs.Accounts.update_user(user, user_params) do
+  @doc false
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Accounts.get_user!(id)
+
+    case Accounts.update_user(user, user_params) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "Profile updated.")
@@ -46,5 +44,4 @@ defmodule Wsdjs.Web.UserController do
         render(conn, "edit.html", user: user, changeset: changeset)
     end
   end
-
 end
