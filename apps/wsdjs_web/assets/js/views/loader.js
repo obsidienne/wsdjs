1ea08bd5a3393346033fc0fe448bb25de3bca29e
claudio bernardes <bernardes.claudio@gmail.com>
2017-07-19 00:53:56 +0200

    Move more code.

--- apps/wsdjs_web/assets/js/views/loader.js
+++ apps/wsdjs_web/assets/js/views/loader.js
@@ -1,6 +1,5 @@
 import MainView    from './main';
 import SongShowView from './song/show';
-import SongIndexView from './song/index';
 import TopVotingView from './top/voting';
 import TopCountingView from './top/counting';
 import UserEditView from './user/edit';
@@ -8,7 +7,6 @@ import UserEditView from './user/edit';
 // Collection of specific view modules
 const views = {
   SongShowView,
-  SongIndexView,
   TopVotingView,
   TopCountingView,
   UserEditView
