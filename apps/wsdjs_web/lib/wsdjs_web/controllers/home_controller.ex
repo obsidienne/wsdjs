83ecbb33fe5973f95ea5bcc6ff526715b2f48b86
claudio bernardes <bernardes.claudio@gmail.com>
2017-07-22 17:57:57 +0200

    Begin home page for unauthenticated users.

--- apps/wsdjs_web/lib/wsdjs_web/controllers/home_controller.ex
+++ apps/wsdjs_web/lib/wsdjs_web/controllers/home_controller.ex
@@ -4,13 +4,20 @@ defmodule Wsdjs.Web.HomeController do
 
   alias Wsdjs.{Musics, Trendings}
 
-  def index(conn, _params) do
-    current_user = conn.assigns[:current_user]
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns[:current_user]]
+    apply(__MODULE__, action_name(conn), args)
+  end
 
+  def index(conn, _params, nil) do
+    render conn, "unauthenticated.html"
+  end
+
+
+  def index(conn, _params, current_user) do
     songs = Musics.hot_songs(current_user)
     top = Trendings.last_top(current_user)
 
     render conn, "index.html", songs: songs, top: top
   end
-
 end
