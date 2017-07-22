19364eadc781ada6694acece1d5e2d65bcb2b3b7
claudio bernardes <bernardes.claudio@gmail.com>
2017-07-22 16:49:17 +0200

    Manage the case where name is not set.

--- apps/wsdjs_web/lib/wsdjs_web/helpers/user_helper.ex
+++ apps/wsdjs_web/lib/wsdjs_web/helpers/user_helper.ex
@@ -5,8 +5,10 @@ defmodule Wsdjs.Web.UserHelper do
   import Wsdjs.Web.Router.Helpers
   alias Wsdjs.Accounts.User
 
-  def user_displayed_name(%User{name: name, djname: djname}) when is_binary(djname), do: "#{name} (#{djname})"
-  def user_displayed_name(%User{name: name}), do: name
+  def user_displayed_name(%User{name: name, djname: djname}) when is_binary(djname) and is_binary(name), do: "#{name} (#{djname})"
+  def user_displayed_name(%User{djname: djname}) when is_binary(djname), do: "#{djname}"
+  def user_displayed_name(%User{name: name}) when is_binary(name), do: name
+  def user_displayed_name(%User{email: email}), do: email
 
   def user_avatar_alt(user), do: "#{user.name}"
 
