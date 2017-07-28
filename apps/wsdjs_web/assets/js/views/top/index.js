637e8d7b3bb4350197425dd1a7c137b66c99ae86
claudio bernardes <bernardes.claudio@gmail.com>
2017-07-28 15:31:20 +0100

    Date internationalisation.

--- apps/wsdjs_web/assets/js/views/top/index.js
+++ apps/wsdjs_web/assets/js/views/top/index.js
@@ -13,6 +13,15 @@ export default class View {
 
     // tooltip
     this.tips = new Tippy(".tippy[title]", {performance: true, size: "small", position: "top", appendTo: document.body});
+
+    // intl date
+    var options = {year: "numeric", month: "long"};
+    var dateTimeFormat = new Intl.DateTimeFormat(undefined, options);
+    var elements = document.querySelectorAll("time");
+    for (let i = 0; i < elements.length; i++) {
+      let datetime = Date.parse(elements[i].getAttribute("datetime"))
+      elements[i].textContent = dateTimeFormat.format(datetime);
+    }
   }
 
   unmount() {
