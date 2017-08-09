f1ccea31d5c62d6f3dbbf0056fb75093767d92bd
claudio bernardes <bernardes.claudio@gmail.com>
2017-08-09 17:45:53 +0200

    Improve image hero.

--- apps/wsdjs_web/lib/wsdjs_web/helpers/cloudinary_helper.ex
+++ apps/wsdjs_web/lib/wsdjs_web/helpers/cloudinary_helper.ex
@@ -7,7 +7,7 @@ defmodule WsdjsWeb.CloudinaryHelper do
   alias Wsdjs.Accounts.Avatar
 
   @art_root_url "//res.cloudinary.com/don2kwaju/image/upload/w_auto/c_limit,w_400/c_crop,g_custom,q_auto,f_auto/"
-  @art_blured_root_url "//res.cloudinary.com/don2kwaju/image/upload/w_400,c_crop,g_custom,f_auto,q_auto/e_blur:300,o_30/"
+  @art_blured_root_url "//res.cloudinary.com/don2kwaju/image/upload/w_400,c_crop,g_custom,f_auto,q_auto/o_30/"
   @art_missing_url "//res.cloudinary.com/don2kwaju/image/upload/v1449164620/wsdjs/missing_cover.jpg"
 
   def art_url(%Art{cld_id: cld_id, version: version}) when is_binary(cld_id) do
