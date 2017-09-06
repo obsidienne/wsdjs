704e36f7a175a71fd7b95a46a8ec0399c7d00438
Claudio Bernardes <bernardes.claudio@gmail.com>
2017-09-06 22:18:51 +0200

    Send an email when an invitation is requested.

--- apps/wsdjs_web/lib/wsdjs_web/controllers/invitation_controller.ex
+++ apps/wsdjs_web/lib/wsdjs_web/controllers/invitation_controller.ex
@@ -5,6 +5,7 @@ defmodule WsdjsWeb.InvitationController do
 
   alias Wsdjs.Accounts
   alias Wsdjs.Accounts.Invitation
+  alias WsdjsWeb.{InvitationEmail, Mailer}
 
   def new(conn, _params) do
     changeset = Accounts.change_invitation(%Invitation{})
@@ -14,7 +15,9 @@ defmodule WsdjsWeb.InvitationController do
   def create(conn, %{"invitation" => params}) do
     case Accounts.create_invitation(params) do
       {:ok, invitation} ->
-        WsdjsWeb.InvitationEmail.invitation_registered(invitation)
+        invitation
+        |> InvitationEmail.invitation_registered()
+        |> Mailer.deliver_later()
 
         conn
         |> put_flash(:info, "Your request has been sent to our administrator who will reply soon.")
