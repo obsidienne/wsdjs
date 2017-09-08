478b789568402c7d1b9565520ac3ffa67f0ef7c7
claudio bernardes <bernardes.claudio@gmail.com>
2017-09-08 09:38:47 +0200

    More_ex_machina

--- apps/wsdjs/test/wsdjs/charts/rank_test.exs
+++ apps/wsdjs/test/wsdjs/charts/rank_test.exs
@@ -5,7 +5,7 @@ defmodule Wsdjs.RankTest do
 
   test "rank song must exist" do
     params = %{top_id: Ecto.UUID.generate(), song_id: Ecto.UUID.generate()}
-    rank = Rank.changeset(%Rank{}, params)
+    rank = Rank.changeset(%Rank{}, params_for(:top))
     assert {:error, %{errors: [song: {"does not exist", _}]}} = Repo.insert(rank)
   end
 end
--- apps/wsdjs/test/wsdjs/charts/top_test.exs
+++ apps/wsdjs/test/wsdjs/charts/top_test.exs
@@ -6,16 +6,13 @@ defmodule Wsdjs.TopTest do
   alias Wsdjs.Repo
 
   describe "changeset" do
-    @create_attrs %{status: "checking", due_date: "2012-06-30"}
-
     test "changeset with minimal valid attributes" do
-      changeset = Top.changeset(%Top{}, @create_attrs)
+      changeset = Top.changeset(%Top{}, params_for(:top))
       assert changeset.valid?
     end
 
     test "top owner accout must exist" do
-      params = Map.put(@create_attrs, :user_id, Ecto.UUID.generate())
-      top = Top.changeset(%Top{}, params)
+      top = Top.changeset(%Top{}, params_for(:top, %{user_id: Ecto.UUID.generate()}))
       assert {:error, %{errors: [user: {"does not exist", _}]}} = Repo.insert(top)
     end
   end
