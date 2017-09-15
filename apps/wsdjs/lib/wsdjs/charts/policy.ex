5350164fb55ea089e6d0f885695631249003ad95
claudio bernardes <bernardes.claudio@gmail.com>
2017-09-15 08:54:56 +0200

    Simplify charts policy

--- apps/wsdjs/lib/wsdjs/charts/policy.ex
+++ apps/wsdjs/lib/wsdjs/charts/policy.ex
@@ -5,12 +5,9 @@ defmodule Wsdjs.Charts.Policy do
   alias Wsdjs.Accounts.User
   alias Wsdjs.Charts.Top
 
-  def can?(%User{admin: :true}, :create_top), do: :ok
-  def can?(%User{admin: :true}, :delete_top), do: :ok
-  def can?(%User{admin: :true}, :stats_top), do: :ok
+  def can?(%User{admin: :true}, _), do: :ok
   def can?(_, _), do: {:error, :unauthorized}
 
-  def can?(%User{admin: :true}, :delete_top, %Top{}), do: :ok
-  def can?(%User{admin: :true}, :update_top, %Top{}), do: :ok
+  def can?(%User{admin: :true}, _, %Top{}), do: :ok
   def can?(_, _, _), do: {:error, :unauthorized}
 end
