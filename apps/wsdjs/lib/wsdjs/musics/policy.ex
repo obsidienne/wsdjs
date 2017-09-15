8286e13b5a195948824e6e7582d196cb48f2fdaa
claudio bernardes <bernardes.claudio@gmail.com>
2017-09-15 23:17:49 +0200

    More correction

--- apps/wsdjs/lib/wsdjs/musics/policy.ex
+++ apps/wsdjs/lib/wsdjs/musics/policy.ex
@@ -2,7 +2,7 @@ defmodule Wsdjs.Musics.Policy do
   alias Wsdjs.Accounts.User
   alias Wsdjs.Musics.Song
   alias Wsdjs.Repo
-  
+
   def can?(%User{admin: true}, _, _), do: :ok
   def can?(%User{id: id}, _, %Song{user_id: id}), do: :ok
   def can?(_, :show, %Song{public_track: true}), do: :ok
@@ -18,6 +18,7 @@ defmodule Wsdjs.Musics.Policy do
   def can?(_, _, _), do: {:error, :unauthorized}
 
   def can?(%User{profil_djvip: true}, :create_comment), do: :ok
+  def can?(%User{profil_djvip: true}, :create_song), do: :ok
   def can?(%User{}, :search), do: :ok
   def can?(%User{}, :list_user_suggestions), do: :ok
 
