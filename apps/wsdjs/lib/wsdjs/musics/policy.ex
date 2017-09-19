902ff617599d74a01670d2734cbf054cac1c0008
claudio bernardes <bernardes.claudio@gmail.com>
2017-09-19 18:34:15 +0200

    Policy correction

--- apps/wsdjs/lib/wsdjs/musics/policy.ex
+++ apps/wsdjs/lib/wsdjs/musics/policy.ex
@@ -17,7 +17,7 @@ defmodule Wsdjs.Musics.Policy do
   end
   def can?(_, _, _), do: {:error, :unauthorized}
 
-  def can?(%User{profil_djvip: true}, :create_comment), do: :ok
+  def can?(%User{}, :create_comment), do: :ok
   def can?(%User{profil_djvip: true}, :create_song), do: :ok
   def can?(%User{}, :search), do: :ok
   def can?(%User{}, :list_user_suggestions), do: :ok
