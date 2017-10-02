2a4261cd1de8aed598a9df8eb0e033d7d269527b
claudio bernardes <bernardes.claudio@gmail.com>
2017-10-02 08:37:32 +0200

    Apply coding style (waiting for Elixir1.6 auto formatter)

--- apps/wsdjs_web/lib/wsdjs_web/controllers/api/comment_controller.ex
+++ apps/wsdjs_web/lib/wsdjs_web/controllers/api/comment_controller.ex
@@ -1,17 +1,17 @@
 defmodule WsdjsWeb.Api.CommentController do
-    @moduledoc false
-    use WsdjsWeb, :controller
+  @moduledoc false
+  use WsdjsWeb, :controller
 
-    alias Wsdjs.Musics
-    alias Wsdjs.Musics.{Comment, Song}
+  alias Wsdjs.Musics
+  alias Wsdjs.Musics.{Comment, Song}
 
-    def index(conn, %{"song_id" => song_id}) do
-        with song <- Musics.get_song!(song_id) do
-            comments = Musics.list_comments(song)
-            conn
-            |> put_status(:created)
-            |> render("index.json", comments: comments)
-        end
+  def index(conn, %{"song_id" => song_id}) do
+    with song <- Musics.get_song!(song_id) do
+      comments = Musics.list_comments(song)
+
+      conn
+      |> put_status(:created)
+      |> render("index.json", comments: comments)
     end
   end
-  
+end
