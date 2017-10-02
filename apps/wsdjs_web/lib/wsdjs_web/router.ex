fc7eecc7208735c6877fc82aed3e95eeeea6c5ac
Phets <pphommar@gmail.com>
2017-10-02 17:48:04 +0200

    Duplicate current api to api/v1

--- apps/wsdjs_web/lib/wsdjs_web/router.ex
+++ apps/wsdjs_web/lib/wsdjs_web/router.ex
@@ -90,6 +90,8 @@ defmodule WsdjsWeb.Router do
     end
 
     scope "/v1", alias: Api.V1 do
+      resources "/now_playing", NowPlayingController, only: [:index]
+      resources "/mobile_config", MobileConfigController, only: [:index]
       resources "/songs", SongController, only: [] do
         resources "/comments", CommentController, only: [:index]
       end
