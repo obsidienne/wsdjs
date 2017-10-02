0eeedb3e7869dd7d8151db7cb27d6e9c77b39fd2
claudio bernardes <bernardes.claudio@gmail.com>
2017-10-02 08:42:53 +0200

    Style on view

--- apps/wsdjs_web/lib/wsdjs_web/views/api/v1/comment_view.ex
+++ apps/wsdjs_web/lib/wsdjs_web/views/api/v1/comment_view.ex
@@ -5,20 +5,25 @@ defmodule WsdjsWeb.Api.V1.CommentView do
   alias WsdjsWeb.CloudinaryHelper
 
   def render("index.json", %{comments: comments}) do
-    %{data: render_many(comments, CommentView, "comment.json")}
+    %{
+      data: render_many(comments, CommentView, "comment.json")
+    }
   end
 
   def render("show.json", %{comment: comment}) do
-    %{data: render_one(comment, CommentView, "comment.json")}
+    %{
+      data: render_one(comment, CommentView, "comment.json")
+    }
   end
 
   def render("comment.json", %{comment: comment}) do
-    %{id: comment.id,
+    %{
+      id: comment.id,
       text: comment.text,
       commented_by_path: user_path(WsdjsWeb.Endpoint, :show, comment.user),
       commented_by: comment.user.name,
       commented_by_avatar: CloudinaryHelper.avatar_url(comment.user.avatar),
       commented_at: DateTime.to_iso8601(DateTime.from_naive!(comment.inserted_at, "Etc/UTC"))
-      }
+    }
   end
 end
