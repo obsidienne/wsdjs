02adb18f1756b0803d2535bbdbd60c1221405111
claudio bernardes <bernardes.claudio@gmail.com>
2017-10-05 23:23:08 +0200

    Add index videos to api.

--- apps/wsdjs_web/lib/wsdjs_web/controllers/api/v1/video_controller.ex
+++ apps/wsdjs_web/lib/wsdjs_web/controllers/api/v1/video_controller.ex
@@ -7,6 +7,14 @@ defmodule WsdjsWeb.Api.V1.VideoController do
 
   action_fallback WsdjsWeb.Api.V1.FallbackController
 
+  def index(conn, %{"song_id" => song_id}) do
+    with song <- Musics.get_song!(song_id) do
+      comments = Musics.list_videos(song)
+
+      render(conn, "index.json", videos: videos)
+    end
+  end
+
   def create(conn, %{"song_id" => song_id, "video" => params}) do
     current_user = conn.assigns[:current_user]
 
