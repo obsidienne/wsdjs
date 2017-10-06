3a725e1f2b0c6ddf1d6cb45e270d72ea7ac960fd
claudio bernardes <bernardes.claudio@gmail.com>
2017-10-06 09:18:49 +0200

    device detection correction

--- apps/wsdjs_web/lib/wsdjs_web/auth/identify_ua.ex
+++ apps/wsdjs_web/lib/wsdjs_web/auth/identify_ua.ex
@@ -7,22 +7,26 @@ defmodule WsdjsWeb.IdentifyUa do
   @doc false
   def call(conn, _repo) do
     conn
-    |> assign(:layout_type, is_mobile(conn))
+    |> assign(:layout_type, device_type(conn))
     |> Phoenix.Controller.put_layout(layout_type(conn))
   end
 
   def layout_type(%Plug.Conn{} = conn) do
-    if is_mobile(conn) == "mobile" do
+    if device_type(conn) == "mobile" do
       {WsdjsWeb.LayoutView, :"mobile-app"}
     else
       {WsdjsWeb.LayoutView, :app}
     end
   end
 
-  def is_mobile(%Plug.Conn{} = conn) do
-    [ua] = get_req_header(conn, "user-agent")
+  def device_type(%Plug.Conn{} = conn) do
+    ua = get_req_header(conn, "user-agent")
+    device_type(ua)
+  end
+  def device_type([ua|_]), do: device_type(ua)
+  def device_type([]), do: "desktop"
+  def device_type(ua) when is_binary(ua) do
     parsed = UAInspector.parse(ua)
-    IO.inspect parsed
     case parsed.device do
       %{type: "smartphone"} -> "mobile"
       _ -> "desktop"
