d6618dcac4119ad5bdf5626eadcbf62d412ba150
claudio bernardes <bernardes.claudio@gmail.com>
2017-10-10 22:22:30 +0200

    Config from distillery help page.

--- apps/wsdjs_web/config/prod.exs
+++ apps/wsdjs_web/config/prod.exs
@@ -18,7 +18,10 @@ config :wsdjs_web, WsdjsWeb.Endpoint,
   url: [host: "www.worldswingdjs.com", port: 443, scheme: "https"],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  server: true,
+  root: ".",
+  version: Application.spec(:myapp, :vsn)
 
 config :wsdjs_web, WsdjsWeb.Mailer,
     adapter: Bamboo.SendgridAdapter,
