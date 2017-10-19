eb089c09118c250eecf34d2eaf99064c116b1641
claudio bernardes <bernardes.claudio@gmail.com>
2017-10-19 20:38:04 +0200

    Clean update

--- apps/wsdjs_web/lib/wsdjs_web/controllers/api/v1/account_controller.ex
+++ apps/wsdjs_web/lib/wsdjs_web/controllers/api/v1/account_controller.ex
@@ -2,15 +2,13 @@ defmodule WsdjsWeb.Api.V1.AccountController do
   @moduledoc false
   use WsdjsWeb, :controller
 
-  plug :put_layout, "login.html"
-
   alias Wsdjs.Accounts
   alias Wsdjs.Accounts.User
 
   action_fallback WsdjsWeb.Api.V1.FallbackController
 
   def show(conn, %{"id" => id}) do
-    with user <- Accounts.get_user!(id) do
+    with %User{} = user <- Accounts.get_user!(id) do
       render(conn, "show.json", user: user)
     end
   end
@@ -18,19 +16,19 @@ defmodule WsdjsWeb.Api.V1.AccountController do
   def show(conn, %{}) do
     current_user = conn.assigns[:current_user]
 
-    with user <- Accounts.get_user!(current_user.id) do
+    with %User{} = user <- Accounts.get_user!(current_user.id) do
       render(conn, "show.json", user: user)
     end
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
     current_user = conn.assigns[:current_user]
-    user = Accounts.get_user(id)
 
-    with :ok <- Accounts.Policy.can?(current_user, :edit_user, user),
-        {:ok, user} <- Accounts.update_user(user, user_params) do
-      [avatar] = Accounts.get_avatar(user)
-      render(conn, "show.json", user: user, avatar: avatar)
+    with user <- Accounts.get_user!(id),
+         :ok <- Accounts.Policy.can?(current_user, :edit_user, user),
+         {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
+
+      render(conn, "show.json", user: user)
     end
   end
 end
