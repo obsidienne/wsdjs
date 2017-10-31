bbe4a2898d6f0d321df9e39285b9a4ec65be1cdb
Claudio Bernardes <bernardes.claudio@gmail.com>
2017-10-31 14:30:14 +0100

    Refactoring.

--- apps/wsdjs_web/lib/wsdjs_web/controllers/api/v1/opinion_controller.ex
+++ apps/wsdjs_web/lib/wsdjs_web/controllers/api/v1/opinion_controller.ex
@@ -3,9 +3,8 @@ defmodule WsdjsWeb.Api.V1.OpinionController do
   use WsdjsWeb, :controller
 
   alias Wsdjs.Musics
+  alias Wsdjs.Musics.Song
   alias Wsdjs.Reactions
-
-  plug :put_layout, false when action in [:create, :delete]
 
   action_fallback WsdjsWeb.Api.V1.FallbackController
   
@@ -22,22 +21,28 @@ defmodule WsdjsWeb.Api.V1.OpinionController do
   def create(conn, %{"kind" => kind, "song_id" => song_id}) do
     current_user = conn.assigns[:current_user]
 
-    song = Musics.get_song!(song_id)
-    Reactions.upsert_opinion(current_user, song, kind)
-    opinions = Reactions.list_opinions(song)
+    with song <- Musics.get_song!(song_id),
+         :ok <- Reactions.Policy.can?(current_user, :create_opinion, song),
+         {:ok, %Reactions.Opinion{}} <- Reactions.upsert_opinion(current_user, song, kind) do
 
-    render(conn, "index.json", song: song, opinions: opinions, current_user: current_user)
+      opinions = Reactions.list_opinions(song)
+
+      conn
+      |> put_status(:created)
+      |> render("index.json", song: song, opinions: opinions, current_user: current_user)
+    end
   end
 
   def delete(conn, %{"id" => id}) do
     current_user = conn.assigns[:current_user]
 
-    opinion = Reactions.get_opinion!(id)
-    {:ok, opinion} = Reactions.delete_opinion(opinion)
+    with opinion <- Reactions.get_opinion!(id),
+         :ok <- Reactions.Policy.can?(current_user, :delete, opinion),
+         {:ok, opinion} = Reactions.delete_opinion(opinion) do
 
-    song = Musics.get_song!(opinion.song_id)
-    opinions = Reactions.list_opinions(song)
-
-    render(conn, "index.json", song: song, opinions: opinions, current_user: current_user)
+      song = Musics.get_song!(opinion.song_id)
+      opinions = Reactions.list_opinions(song)
+      render(conn, "index.json", song: song, opinions: opinions, current_user: current_user)
+    end
   end
 end
