dfd67cf8d1ed2351cb608a0a4e4ba5da8c384138
Claudio Bernardes <bernardes.claudio@gmail.com>
2017-11-01 18:23:09 +0100

    Remove validate_url as it does not work with docker image

--- apps/wsdjs/lib/wsdjs/attachments/video.ex
+++ apps/wsdjs/lib/wsdjs/attachments/video.ex
@@ -22,18 +22,7 @@ defmodule Wsdjs.Attachments.Video do
     |> cast(attrs, [:url, :event, :title, :user_id, :song_id])
     |> validate_required(:url)
     |> assoc_constraint(:user)
-    |> validate_url(:url)
     |> assoc_constraint(:song)
     |> put_change(:video_id, Wsdjs.Helpers.Provider.extract(attrs["url"]))
-  end
-
-  # This function validates the format of an URL not it's validity.
-  defp validate_url(changeset, field, options \\ []) do
-    validate_change changeset, field, fn _, url ->
-      case url |> String.to_charlist |> :http_uri.parse do
-        {:ok, _} -> []
-        {:error, msg} -> [{field, options[:message] || "invalid url: #{inspect msg}"}]
-      end
-    end
   end
 end
