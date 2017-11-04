ee0670658529a6f42c57a6c11a72b8bcef476e93
claudio bernardes <bernardes.claudio@gmail.com>
2017-11-04 13:59:06 +0100

    Bump version

--- apps/wsdjs/mix.exs
+++ apps/wsdjs/mix.exs
@@ -4,7 +4,7 @@ defmodule Wsdjs.Mixfile do
   def project do
     [
       app: :wsdjs,
-      version: "1.5",
+      version: "1.5.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
