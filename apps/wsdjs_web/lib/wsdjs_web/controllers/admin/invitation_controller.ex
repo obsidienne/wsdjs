001a5267fc0d236f677e213d1d20d424636bfe42
claudio bernardes <bernardes.claudio@gmail.com>
2017-11-13 22:54:07 +0100

    Send an email when an invitation is validated by an administrator.

--- apps/wsdjs_web/lib/wsdjs_web/controllers/admin/invitation_controller.ex
+++ apps/wsdjs_web/lib/wsdjs_web/controllers/admin/invitation_controller.ex
@@ -5,10 +5,16 @@ defmodule WsdjsWeb.Admin.InvitationController do
   use WsdjsWeb, :controller
 
   alias Wsdjs.Accounts
+  alias WsdjsWeb.{InvitationEmail, Mailer}
 
   def update(conn, %{"id" => id}) do
     with invitation <- Accounts.get_invitation!(id),
          {:ok, invitation} <- Accounts.accept_invitation(invitation) do
+
+      # mailing is not in the with because it's not a blocking
+      invitation
+      |> InvitationEmail.invitation_accepted()
+      |> Mailer.deliver_later()
 
       conn
       |> put_flash(:info, "Invitation #{invitation.name} accepted successfully.")
