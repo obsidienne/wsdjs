4ccc8d7b4a98a601c0f738e450dab13b5946a905
claudio bernardes <bernardes.claudio@gmail.com>
2017-11-17 08:43:10 +0100

    Template path correction for new song notification

--- apps/wsdjs_jobs/lib/wsdjs_jobs/jobs/new_song_notification.ex
+++ apps/wsdjs_jobs/lib/wsdjs_jobs/jobs/new_song_notification.ex
@@ -1,19 +1,20 @@
 defmodule Wsdjs.Jobs.NewSongNotification do
   import Bamboo.Email
 
-  @html_mail_template Path.expand("./lib/wsdjs_jobs/priv/static/email/new_song_notification.html.eex")
-  @text_mail_template Path.expand("./lib/wsdjs_jobs/priv/static/email/new_song_notification.text.eex")
-
   def call(_args \\ []) do
     users = Wsdjs.Accounts.list_users_to_notify("new song")
+
+    base_tpl = Path.join(["#{:code.priv_dir(:wsdjs_jobs)}", "static", "email"])
+    html_tpl = Path.join([base_tpl, "radioking_unmatch.html.eex"])
+    txt_tpl = Path.join([base_tpl, "radioking_unmatch.html.eex"])
 
     if Enum.count(users) > 0 do
       new_email()
       |> bcc(users)
       |> from("no-reply@wsdjs.com")
       |> subject("New songs suggested")
-      |> html_body(EEx.eval_file(@html_mail_template))
-      |> text_body(EEx.eval_file(@text_mail_template))
+      |> html_body(EEx.eval_file(html_tpl))
+      |> text_body(EEx.eval_file(txt_tpl))
       |> Wsdjs.Jobs.Mailer.deliver_later
     end
   end
