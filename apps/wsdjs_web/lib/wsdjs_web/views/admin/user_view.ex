f1c0049a3e998d0f5a456756e6a467e0f17e2c58
claudio bernardes <bernardes.claudio@gmail.com>
2017-11-20 18:23:40 +0100

    Show a data to link the user when only the email is filled

--- apps/wsdjs_web/lib/wsdjs_web/views/admin/user_view.ex
+++ apps/wsdjs_web/lib/wsdjs_web/views/admin/user_view.ex
@@ -1,3 +1,9 @@
 defmodule WsdjsWeb.Admin.UserView do
   use WsdjsWeb, :view
+
+  alias Wsdjs.Accounts.User
+
+  def name_or_email(%User{email: email, name: nil, djname: nil}), do: email
+  def name_or_email(%User{email: email, name: name}) when is_binary(name), do: name
+  def name_or_email(%User{email: email, djname: djname}) when is_binary(djname), do: djname
 end
