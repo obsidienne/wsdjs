334d32693ab60352171004a365184bb301c111df
claudio bernardes <bernardes.claudio@gmail.com>
2017-12-07 09:22:56 +0100

    Remove factory

--- apps/wsdjs/test/wsdjs/reactions/reactions_test.exs
+++ apps/wsdjs/test/wsdjs/reactions/reactions_test.exs
@@ -1,19 +1,26 @@
 defmodule Wsdjs.ReactionsTest do
   use Wsdjs.DataCase
-  import Wsdjs.Factory
   alias Wsdjs.Reactions
-  alias Wsdjs.Repo
 
   describe "comments" do
+    def comment_fixture() do
+      {:ok, user} = Wsdjs.Accounts.create_user(%{email: "dummy#{System.unique_integer()}@bshit.com"})
+      {:ok, song} = Wsdjs.Musics.create_song(%{title: "my title#{System.unique_integer()}", artist: "my artist", genre: "soul", url: "http://youtu.be/dummy", user_id: user.id})
+      {:ok, comment} = Reactions.create_comment(%{text: "mega song", user_id: user.id, song_id: song.id})
+
+      comment
+    end
+
     test "get_comment!/1 returns the comment with given id" do
-      comment = insert(:comment)
-      assert comment == Reactions.get_comment!(comment.id) |> Repo.preload([:user, song: :user])
+      comment = comment_fixture()
+      assert Reactions.get_comment!(comment.id) |> Wsdjs.Repo.preload(user: :avatar) == comment
     end
 
     test "list_comments/1 returns all song comments" do
-      song = insert(:song)
-      comments = insert_list(3, :comment, song: song)
-      assert Reactions.list_comments(song) |> Repo.preload(song: :user) |> Enum.sort() == comments |> Repo.preload([user: :avatar]) |> Enum.sort()
+      comment = comment_fixture()
+      _dummy = comment_fixture()
+      song = Wsdjs.Musics.get_song!(comment.song_id)
+      assert Reactions.list_comments(song) == [comment]
     end
   end
 end
