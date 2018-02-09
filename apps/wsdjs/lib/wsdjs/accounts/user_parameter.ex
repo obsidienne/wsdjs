5f336c59170f6d90102a806178700dad452bbb79
claudio bernardes <bernardes.claudio@gmail.com>
2018-02-09 23:30:06 +0100

    format

--- apps/wsdjs/lib/wsdjs/accounts/user_parameter.ex
+++ apps/wsdjs/lib/wsdjs/accounts/user_parameter.ex
@@ -6,14 +6,14 @@ defmodule Wsdjs.Accounts.UserParameter do
 
   @foreign_key_type :binary_id
   schema "user_parameters" do
-    field :new_song_notification, :boolean, default: false
-    field :piwik, :boolean, default: true
-    field :video, :boolean, default: false
-    field :radioking_unmatch, :boolean, default: false
-    field :email_contact, :boolean, default: false
-    field :public_top_like, :boolean, default: false
+    field(:new_song_notification, :boolean, default: false)
+    field(:piwik, :boolean, default: true)
+    field(:video, :boolean, default: false)
+    field(:radioking_unmatch, :boolean, default: false)
+    field(:email_contact, :boolean, default: false)
+    field(:public_top_like, :boolean, default: false)
 
-    belongs_to :user, Wsdjs.Accounts.User
+    belongs_to(:user, Wsdjs.Accounts.User)
 
     timestamps()
   end
@@ -28,7 +28,14 @@ defmodule Wsdjs.Accounts.UserParameter do
   @doc false
   def admin_changeset(%UserParameter{} = user_parameter, attrs) do
     user_parameter
-    |> cast(attrs, [:new_song_notification, :piwik, :video, :radioking_unmatch, :email_contact, :public_top_like])
+    |> cast(attrs, [
+      :new_song_notification,
+      :piwik,
+      :video,
+      :radioking_unmatch,
+      :email_contact,
+      :public_top_like
+    ])
     |> assoc_constraint(:user)
   end
 end
