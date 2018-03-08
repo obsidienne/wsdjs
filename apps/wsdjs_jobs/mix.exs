f29776053fe901fa72200703dddaf65f4e072fea
Claudio Bernardes <bernardes.claudio@gmail.com>
2018-03-09 00:17:08 +0100

    downgrade elixir version

--- apps/wsdjs_jobs/mix.exs
+++ apps/wsdjs_jobs/mix.exs
@@ -9,7 +9,7 @@ defmodule Wsdjs.Jobs.Mixfile do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "1.6.1",
+      elixir: "1.5.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
