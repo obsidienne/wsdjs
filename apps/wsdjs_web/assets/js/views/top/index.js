fc9d57ceff8968caa215980d0f2437eb194ad0e9
claudio bernardes <bernardes.claudio@gmail.com>
2018-03-08 19:55:34 +0100

    use intersection observer and fetch

--- apps/wsdjs_web/assets/js/views/top/index.js
+++ apps/wsdjs_web/assets/js/views/top/index.js
@@ -4,79 +4,45 @@ export default class View extends MainView {
   constructor() {
     super();
     
-    var self = this;
-    var timeout;
-    window.addEventListener("scroll", function(e) {
-      clearTimeout(timeout);
-      timeout = setTimeout(function() {
-        if (self._needToFetchTops()) {
-          var sentinel = document.querySelector("#top-list .sentinel");
-          sentinel.parentNode.removeChild(sentinel);    
-          self._fetchTops();
+    this.observer = new IntersectionObserver((entries) => { 
+      for (var i = 0; i < entries.length; i++) {
+        if (entries[i].isIntersecting) {
+          let sentinel = entries[i].target;
+          this.observer.unobserve(sentinel);
+          this.fetchTops(sentinel);
         }
-      }, 100);
-    })
-  }
-
-  mount() { 
-    super.mount();
-  }
-
-  unmount() {
-    super.umount();
-  }
-
-  _formatDate() {
-    super.formatDate("time.date-format", {year: "numeric", month: "long"});
-  }
-
-  _needToFetchTops() {
-    /* check the sentinel is in DOM */
-    var correctPage = document.querySelector(".TopIndexView");
-    var sentinel = document.querySelector("#top-list .sentinel");
-    if (!sentinel) return false;
-    
-    /* check nb pages already fetched */
-    var container = document.getElementById("top-list");
-    var page_number = parseInt(container.dataset.jsPageNumber);
-    var page_total = parseInt(container.dataset.jsTotalPages);
-    if (page_number >= page_total) return false;
-    
-    /* check sentinel is in viewport*/
-    var rect = sentinel.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document. documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document. documentElement.clientWidth) &&
-      correctPage
-    );
-  }
-
-  _fetchTops() {
-    var container = document.getElementById("top-list");
-    var page_number = parseInt(container.dataset.jsPageNumber);
-    
-    var self = this;
-    var request = new XMLHttpRequest();
-    request.open('GET', `/tops?page=${page_number + 1}`, true);
-
-    request.onload = function() {
-      if (this.status >= 200 && this.status < 400) {
-        var total_pages = request.getResponseHeader("total-pages");
-        var page_number = request.getResponseHeader("page-number");
-
-        container.dataset.jsPageNumber = page_number;
-        container.dataset.jsTotalPages = total_pages;
-
-        container.insertAdjacentHTML('beforeend', this.response);
-
-        self._formatDate();
       }
-    };
+    });
 
-    request.onerror = function() { console.log("Error search"); };
+    var sentinel = document.querySelector("#top-list .sentinel");
+    this.observer.observe(sentinel);
+  }
 
-    request.send();    
+  fetchTops(sentinel) {
+    var container = document.getElementById("top-list");
+    var page_number = parseInt(sentinel.dataset.jsPageNumber);
+    
+    fetch(`/tops?page=${page_number + 1}`, {credentials: 'include'})
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      } else {
+        console.log('Mauvaise réponse du réseau');
+      }
+    })
+    .then((data) => {
+      var sentinel = document.querySelector("#top-list .sentinel");
+      sentinel.parentNode.removeChild(sentinel);
+
+      container.insertAdjacentHTML('beforeend', data);
+
+      var sentinel = document.querySelector("#top-list .sentinel");
+      this.observer.observe(sentinel);
+
+      super.formatDate("time.date-format", {year: "numeric", month: "long"});
+    })
+    .catch(function(error) {
+      console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+    });
   }
 }
