41dbbbfdef071ea15f2584b0465a83008cdf9db9
claudio bernardes <bernardes.claudio@gmail.com>
2018-03-12 12:46:22 +0100

    Dump version

--- apps/wsdjs_web/mix.exs
+++ apps/wsdjs_web/mix.exs
@@ -4,7 +4,7 @@ defmodule WsdjsWeb.Mixfile do
   def project do
     [
       app: :wsdjs_web,
-      version: "1.8.0",
+      version: "1.8.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
