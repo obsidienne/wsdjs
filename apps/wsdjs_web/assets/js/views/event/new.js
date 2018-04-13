fa849194591ce3507592783a04aafd5dca8a1b25
claudio bernardes <bernardes.claudio@gmail.com>
2018-04-13 19:53:01 +0200

    Fill form on create

--- apps/wsdjs_web/assets/js/views/event/new.js
+++ apps/wsdjs_web/assets/js/views/event/new.js
@@ -8,8 +8,14 @@ export default class View extends MainView {
 
   mount() {
     super.mount();
-    places({
-      container: document.querySelector('.search-place')
+    let adress = places({
+      container: document.querySelector('.search-place'),
+      type: "address"
     })
+
+    adress.on('change', e => {
+      document.getElementById("event_lng").value = e.suggestion.latlng.lng;
+      document.getElementById("event_lat").value = e.suggestion.latlng.lat;
+    });
   }
 }
