418a54e7d4f487861da681d2cfd24325b75e6ab2
claudio bernardes <bernardes.claudio@gmail.com>
2018-05-02 08:03:00 +0200

    add earmark for markdown parsing

--- apps/wsdjs_web/mix.exs
+++ apps/wsdjs_web/mix.exs
@@ -46,6 +46,7 @@ defmodule WsdjsWeb.Mixfile do
       {:bamboo, "~> 0.8"},
       {:timex, "~> 3.1"},
       {:corsica, "~> 1.0"},
+      {:earmark, "~> 1.2"},
       {:ua_inspector, "~> 0.16.1"},
       {:wsdjs, in_umbrella: true},
       {:wsdjs_jobs, in_umbrella: true},
