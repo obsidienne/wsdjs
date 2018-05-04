2d7b3756455cc82c44cbc2f9b20cd927b22999c8
claudio bernardes <bernardes.claudio@gmail.com>
2018-05-04 19:17:26 +0200

    Add fb_url to event

--- apps/wsdjs/lib/wsdjs/happenings/event.ex
+++ apps/wsdjs/lib/wsdjs/happenings/event.ex
@@ -12,6 +12,7 @@ defmodule Wsdjs.Happenings.Event do
     field(:name, :string)
     field(:starts_on, :date)
     field(:ends_on, :date)
+    field(:fb_url, :string)
 
     field(:venue, :string)
     field(:coordinates, Geo.Point)     # add the actual column
@@ -26,10 +27,11 @@ defmodule Wsdjs.Happenings.Event do
   @doc false
   def changeset(%Event{} = event, attrs) do
     event
-    |> cast(attrs, [:name, :user_id, :starts_on, :ends_on, :lng, :lat, :venue])
+    |> cast(attrs, [:name, :user_id, :starts_on, :ends_on, :lng, :lat, :venue, :fb_url])
     |> validate_required([:name, :starts_on, :ends_on, :lng, :lat, :venue])
     |> assoc_constraint(:user)
     |> cast_coordinates()
+    |> validate_url(:fb_url)
   end
 
   def cast_coordinates(changeset) do
@@ -37,5 +39,15 @@ defmodule Wsdjs.Happenings.Event do
     lng = get_change(changeset, :lng)
     geo = %Geo.Point{coordinates: {lng, lat}, srid: 4326}
     changeset |> put_change(:coordinates, geo)
+  end
+
+  # This function validates the format of an URL not it's validity.
+  defp validate_url(changeset, field, options \\ []) do
+    validate_change(changeset, field, fn _, url ->
+      case url |> String.to_charlist() |> :http_uri.parse() do
+        {:ok, _} -> []
+        {:error, msg} -> [{field, options[:message] || "invalid url: #{inspect(msg)}"}]
+      end
+    end)
   end
 end
