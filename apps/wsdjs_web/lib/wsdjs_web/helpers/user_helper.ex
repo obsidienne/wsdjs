a06c98a8cc5606785ea9418469b4f6ed5956315f
claudio bernardes <bernardes.claudio@gmail.com>
2018-06-07 19:11:53 +0200

    Modify style

--- apps/wsdjs_web/lib/wsdjs_web/helpers/user_helper.ex
+++ apps/wsdjs_web/lib/wsdjs_web/helpers/user_helper.ex
@@ -20,7 +20,7 @@ defmodule WsdjsWeb.UserHelper do
       user_displayed_name(song.user),
       to: user_path(conn, :show, song.user.id),
       title: "suggested by #{song.user.name}, #{Timex.format!(song.inserted_at, "%b %d, %Y", :strftime)}",
-      class: "visible-link clickable text-ellipsis"
+      class: "visible-link clickable d-inline-block"
     )
   end
 end
