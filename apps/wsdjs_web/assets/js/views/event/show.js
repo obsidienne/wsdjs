d336d895cc65ffbf1d37efcbe8abdb2d10162c39
claudio bernardes <bernardes.claudio@gmail.com>
2018-06-13 08:43:37 +0200

    display leaflet icons

--- apps/wsdjs_web/assets/js/views/event/show.js
+++ apps/wsdjs_web/assets/js/views/event/show.js
@@ -2,9 +2,22 @@ import MainView from '../main';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+// stupid hack so that leaflet's images work after going through webpack
+import marker from 'leaflet/dist/images/marker-icon.png';
+import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
 export default class View extends MainView {
   mount() {
     super.mount();
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: `/js/${marker2x}`,
+      iconUrl: `/js/${marker}`,
+      shadowUrl: `/js/${markerShadow}`
+    });
 
     let el = document.getElementById("mapid");
 
