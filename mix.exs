b654d2bcd0410bc95f441c1854a3d1cdb9c1f1ae
claudio bernardes <bernardes.claudio@gmail.com>
2018-06-14 09:10:48 +0200

    Bump dialyxir version

--- mix.exs
+++ mix.exs
@@ -24,9 +24,9 @@ defmodule Wsdjs.Umbrella.Mixfile do
   # and cannot be accessed from applications inside the apps folder
   defp deps do
     [
-      {:credo, "~> 0.9.0-rc1", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:credo, "~> 0.9.3", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.0", only: [:dev], runtime: false}
     ]
   end
 
