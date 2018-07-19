d50c1eac90a1b58f226c07536a4ec3360407159c
claudio bernardes <bernardes.claudio@gmail.com>
2018-07-19 21:16:19 +0200

    Show tooltip in show

--- apps/wsdjs_web/assets/js/views/song/index.js
+++ apps/wsdjs_web/assets/js/views/song/index.js
@@ -1,5 +1,6 @@
 import MainView from '../main';
 import lazyload from '../../components/lazyload';
+import Tippy from 'tippy.js/dist/tippy.all';
 
 export default class View extends MainView {
   constructor() {
@@ -19,6 +20,60 @@ export default class View extends MainView {
     this.observer.observe(sentinel);
 
     lazyload.refresh();
+  }
+
+  mount() {
+    super.mount();
+    this.tip = this.opinionTooltipMount();
+  }
+
+  unmount() {
+    super.umount();
+    this.tip.destroyAll();
+  }
+
+  opinionTooltipMount() {
+    let tip = new Tippy("#song-list", {
+      animation: 'shift-toward',
+      arrow: true,
+      arrowType: 'round',
+      html: '#opinion-selector-tpl',
+      performance: true,
+      target: ".opinion-tippy",
+      onHidden: (instance) => {
+        const content = instance.popper.querySelector('.tippy-content');
+        content.innerHTML = "loading...";
+      },
+      onShow: (instance) => {
+        const content = instance.popper.querySelector('.tippy-content');
+        console.log(content.innerHTML)
+        if (this.tip.loading || content.innerHTML !== "loading...") return;
+        this.tip.loading = true;
+
+        fetch('https://unsplash.it/200/?random').then(resp => resp.blob()).then(blob => {
+          const url = URL.createObjectURL(blob);
+          content.innerHTML = `<img width="200" height="200" src="${url}">`;
+          this.tip.loading = false;
+        }).catch(e => {
+          content.innerHTML = 'Loading failed';
+          this.tip.loading = false;
+        })
+      },
+      // prevent tooltip from displaying over button
+      popperOptions: {
+        modifiers: {
+          preventOverflow: {
+            enabled: true
+          },
+          hide: {
+            enabled: false
+          }
+        }
+      }
+    });
+
+    tip.loading = false;
+    return tip;
   }
 
   fetchSongs(sentinel) {
