a9c2511dd3e6e4f85b2104a005da02d39a5827f5
claudio bernardes <bernardes.claudio@gmail.com>
2018-08-31 18:17:03 +0200

    Remove dialyxir errors

--- apps/wsdjs_web/lib/wsdjs_web/views/song_videos_view.ex
+++ apps/wsdjs_web/lib/wsdjs_web/views/song_videos_view.ex
@@ -1,9 +1,7 @@
 defmodule WsdjsWeb.SongVideosView do
   use WsdjsWeb, :view
 
-  alias Wsdjs.Accounts
   alias Wsdjs.Happenings
-  alias Wsdjs.Reactions.Opinion
 
   def list_events do
     events = Happenings.list_events()
