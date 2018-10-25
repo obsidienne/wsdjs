92c038f3613987a4b8207f19a742e9cac62ca54d
Claudio Bernardes <bernardes.claudio@gmail.com>
2018-10-25 08:53:42 +0200

    Add a toggleAttribute polyfill

--- apps/wsdjs_web/assets/js/helpers.js
+++ apps/wsdjs_web/assets/js/helpers.js
@@ -20,3 +20,23 @@ window.formToObject = (form) => {
 
     return output;
 }
+
+if (!Element.prototype.toggleAttribute) {
+    Element.prototype.toggleAttribute = function (name, force) {
+        var forcePassed = arguments.length === 2;
+        var forceOn = !!force;
+        var forceOff = forcePassed && !force;
+
+        if (this.getAttribute(name) !== null) {
+            if (forceOn) return true;
+
+            this.removeAttribute(name);
+            return false;
+        } else {
+            if (forceOff) return false;
+
+            this.setAttribute(name, "");
+            return true;
+        }
+    };
+}
