9040a2b527dca03bcf44e10946098cbbabb7e838
Claudio Bernardes <bernardes.claudio@gmail.com>
2018-11-09 18:17:48 +0100

    Bump Elixir version
    Update erlang and elixir version

--- apps/wsdjs_jobs/mix.exs
+++ apps/wsdjs_jobs/mix.exs
@@ -9,7 +9,7 @@ defmodule Wsdjs.Jobs.Mixfile do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "1.6.5",
+      elixir: "1.7.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
