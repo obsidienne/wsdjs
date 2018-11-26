9e0e44b7fc3924af57623c6e9760765219a276c3
Claudio Bernardes <bernardes.claudio@gmail.com>
2018-11-26 19:41:44 +0100

    Bump version and update CHANGELOG

--- apps/wsdjs_web/mix.exs
+++ apps/wsdjs_web/mix.exs
@@ -4,7 +4,7 @@ defmodule WsdjsWeb.Mixfile do
   def project do
     [
       app: :wsdjs_web,
-      version: "1.9.0",
+      version: "1.9.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
