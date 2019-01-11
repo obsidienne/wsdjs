8c77425d74dd28131accb6010decb916fef21f5b
Claudio Bernardes <bernardes.claudio@gmail.com>
2019-01-11 08:49:22 +0100

    Dump version and changelog

--- apps/wsdjs_api/mix.exs
+++ apps/wsdjs_api/mix.exs
@@ -4,7 +4,7 @@ defmodule WsdjsApi.MixProject do
   def project do
     [
       app: :wsdjs_api,
-      version: "0.1.0",
+      version: "2.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
