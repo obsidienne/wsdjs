c88726d5dc4917974ec20616debc18bbd6812154
Claudio Bernardes <bernardes.claudio@gmail.com>
2019-01-11 19:26:29 +0100

    Tiny refactoring

--- apps/wsdjs_api/lib/wsdjs_api/auth/ensure_authenticated.ex
+++ apps/wsdjs_api/lib/wsdjs_api/auth/ensure_authenticated.ex
@@ -7,7 +7,7 @@ defmodule WsdjsApi.EnsureAuthenticated do
     case conn.assigns[:current_user] do
       nil ->
         conn = conn |> halt
-        apply(WsdjsApi.Unauthenticated, :api_call, [conn, conn.params])
+        apply(WsdjsApi.Unauthenticated, :call, [conn, conn.params])
 
       _ ->
         conn
