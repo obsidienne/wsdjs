901f6ffda4215081ba613fd9f97d0a90aed0c0e4
Claudio Bernardes <bernardes.claudio@gmail.com>
2019-01-15 09:03:28 +0100

    Start using tailwindcss
    Remove position.css
    
    
    Remove custom css class
    
    
    Remove inline-flex class
    
    
    Add purgecss
    
    
    always add purgecss
    
    
    rounded-full
    
    
    Remove useless CSS
    
    
    Remove unused border width
    
    
    inline-block
    
    
    Remove display.css
    
    
    Move more CSS declarations to tailwind
    
    
    Move CSS declations to tailwind
    
    
    Do not duplicate normalize.css
    
    
    Replace CSS class by tailwind class
    
    
    remove shame.css
    
    
    Replace CSS class by tailwind class
    
    
    Replace CSS class by tailwind class
    
    
    Replace CSS class by tailwind class
    
    
    Replace CSS class by tailwind CSS
    
    
    Replace CSS class by tailwind class
    
    
    Replace CSS class by tailwind class
    
    
    Replace CSS
    
    
    Replace CSS
    
    
    Replace CSS class by tailwind class
    
    
    Replace CSS class by tailwind class
    
    
    Replace CSS class by tailwind class

--- apps/wsdjs_web/assets/postcss.config.js
+++ apps/wsdjs_web/assets/postcss.config.js
@@ -1,5 +1,12 @@
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
     plugins: [
+        tailwindcss('./tailwind.js'),
+//        purgecss({
+//            content: ['../lib/**/**/*.eex']
+//        }),
         require('autoprefixer')
     ]
 }
