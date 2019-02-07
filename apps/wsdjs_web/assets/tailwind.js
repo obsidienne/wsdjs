889124c65e2a5fe3dbb2e463bb5ee2c45293b807
Claudio Bernardes <bernardes.claudio@gmail.com>
2019-02-07 19:16:48 +0100

    Add aspect-ratio to tailwindcss

--- apps/wsdjs_web/assets/tailwind.js
+++ apps/wsdjs_web/assets/tailwind.js
@@ -956,6 +956,14 @@ module.exports = {
   */
 
   plugins: [
+    require('tailwindcss-aspect-ratio')({
+      ratios: {
+        'square': [1, 1],
+        '16/9': [16, 9],
+        '4/3': [4, 3],
+        '21/9': [21, 9],
+      }
+    })
     /*
     require('tailwindcss/plugins/container')({
       // center: true,
