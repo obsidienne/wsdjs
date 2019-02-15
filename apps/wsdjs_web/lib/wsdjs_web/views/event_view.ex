bfb91d2c6dccd19c918987bc987f24550ef26a3f
Claudio Bernardes <bernardes.claudio@gmail.com>
2019-02-15 23:07:22 +0100

    Events as card

--- apps/wsdjs_web/lib/wsdjs_web/views/event_view.ex
+++ apps/wsdjs_web/lib/wsdjs_web/views/event_view.ex
@@ -3,4 +3,6 @@ defmodule WsdjsWeb.EventView do
 
   def lat(%Geo.Point{coordinates: {_, lat}}), do: lat
   def lon(%Geo.Point{coordinates: {lon, _}}), do: lon
+  def lat(nil), do: ""
+  def lon(nil), do: ""
 end
