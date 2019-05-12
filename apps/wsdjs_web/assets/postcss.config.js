ee80340f99c83d6fd468592e1ae777dd72fb1da2
Claudio Bernardes <bernardes.claudio@gmail.com>
2019-05-12 10:18:10 +0200

    Remove useless CSS

--- apps/wsdjs_web/assets/postcss.config.js
+++ apps/wsdjs_web/assets/postcss.config.js
@@ -1,12 +1,12 @@
-const tailwindcss = require('tailwindcss');
-const purgecss = require('@fullhuman/postcss-purgecss');
+const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-//        purgecss({
-//            content: ['../lib/**/**/*.eex']
-//        }),
-        require('autoprefixer')
-    ]
-}
+  plugins: [
+    tailwindcss("./tailwind.js"),
+    purgecss({
+      content: ["../lib/**/**/*.eex"]
+    }),
+    require("autoprefixer")
+  ]
+};
