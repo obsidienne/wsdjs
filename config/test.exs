eb67829fb0dc965529e15e7b43bc66989c995c4e
Claudio Bernardes <bernardes.claudio@gmail.com>
2020-05-04 00:44:16 +0200

    Merge test

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,22 @@
 use Mix.Config
 
+# Configure your database
+config :wsdjs, Wsdjs.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "wsdjs_test",
+  hostname: "localhost",
+  types: Wsdjs.PostgresTypes,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :wsdjs_web, WsdjsWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+config :wsdjs_web, WsdjsWeb.ApiRouteHelpers, base_url: "http://api:5000"
+config :wsdjs_api, WsdjsApi.WebRouteHelpers, base_url: "http://web:4000"
+
 # Print only warnings and errors during test
 config :logger, level: :warn
