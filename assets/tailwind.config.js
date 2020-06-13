fcb7f2127dc17ca8a93a55d2f75b55b8a7656368
claudio <bernardes.claudio@gmail.com>
2020-06-13 11:50:32 +0200

    Design correction

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
         "../**/live/**/*.ex",
         "./js/**/*.js"
     ],
-    theme: {},
-    variants: {},
-    plugins: []
+    theme: {
+        aspectRatio: { // defaults to {}
+            'none': 0,
+            'square': [1, 1], // or 1 / 1, or simply 1
+            '16/9': [16, 9],  // or 16 / 9
+            '4/3': [4, 3],    // or 4 / 3
+            '21/9': [21, 9],  // or 21 / 9
+        },
+    },
+    variants: {
+        aspectRatio: ['responsive']
+    },
+    plugins: [
+        require('tailwindcss-aspect-ratio')
+    ]
 };
