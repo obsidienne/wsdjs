73a74459cf74cac32c1b545385c959d7aae16537
claudio <bernardes.claudio@gmail.com>
2020-06-13 02:43:30 +0200

    Cleaning

--- lib/wsdjs_web/api_route_helpers.ex
+++ lib/wsdjs_web/api_route_helpers.ex
@@ -2,22 +2,11 @@ defmodule WsdjsWeb.ApiRouteHelpers do
   @doc ~S"""
   ## Examples
 
-      iex> WsdjsWeb.ApiRouteHelpers.base_path()
-      "http://api:5000"
-  """
-  def base_path do
-    Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-  end
-
-  @doc ~S"""
-  ## Examples
-
       iex> WsdjsWeb.ApiRouteHelpers.opinion_path("conn", :delete, %{id: 42})
       "http://api:5000/opinions/42"
   """
   def opinion_path(_, :delete, %{id: id}) do
-    base_url = Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-    base_url <> "/opinions/#{id}"
+    "/api/opinions/#{id}"
   end
 
   @doc ~S"""
@@ -30,13 +19,11 @@ defmodule WsdjsWeb.ApiRouteHelpers do
       "http://api:5000/comments/42"
   """
   def comment_path(_, :delete, %{id: id}) do
-    base_url = Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-    base_url <> "/comments/#{id}"
+    "/api/comments/#{id}"
   end
 
   def comment_path(_, :delete, id) do
-    base_url = Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-    base_url <> "/comments/#{id}"
+    "/api/comments/#{id}"
   end
 
   @doc ~S"""
@@ -46,8 +33,7 @@ defmodule WsdjsWeb.ApiRouteHelpers do
       "http://api:5000/songs/42/comments"
   """
   def song_comment_path(_, :create, %{id: id}) do
-    base_url = Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-    base_url <> "/songs/#{id}/comments"
+    "/api/songs/#{id}/comments"
   end
 
   @doc ~S"""
@@ -57,10 +43,7 @@ defmodule WsdjsWeb.ApiRouteHelpers do
       "http://api:5000/songs/42/opinions?kind=up"
   """
   def song_opinion_path(_, :create, %{id: id}, query) when is_list(query) do
-    base_url = Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-
-    base_url <>
-      "/songs/#{id}/opinions" <>
+    "/api/songs/#{id}/opinions" <>
       case Enum.count(query) do
         0 -> ""
         _ -> "?" <> URI.encode_query(query)
@@ -77,18 +60,15 @@ defmodule WsdjsWeb.ApiRouteHelpers do
       "http://api:5000/videos/42"
   """
   def video_path(_, :delete, %{id: id}) do
-    base_url = Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-    base_url <> "/videos/#{id}"
+    "/api/videos/#{id}"
   end
 
   def video_path(_, :delete, id) do
-    base_url = Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-    base_url <> "/videos/#{id}"
+    "/api/videos/#{id}"
   end
 
   def signin_url(_, :show, token) do
-    base_url = Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-    base_url <> "/signin/#{token}"
+    "/api/signin/#{token}"
   end
 
   @doc ~S"""
@@ -98,7 +78,6 @@ defmodule WsdjsWeb.ApiRouteHelpers do
       "http://api:5000/songs/42/videos"
   """
   def song_video_path(_, :create, %{id: id}) do
-    base_url = Application.get_env(:wsdjs, __MODULE__) |> Keyword.get(:base_url)
-    base_url <> "/songs/#{id}/videos"
+    "/api/songs/#{id}/videos"
   end
 end
