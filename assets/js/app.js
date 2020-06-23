b9fdf86ec681498934eaec2750651a7da7ade143
Claudio Bernardes <bernardes.claudio@gmail.com>
2020-06-23 23:02:04 +0200

    Add alpinejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,10 +16,18 @@ import "tippy.js/dist/tippy.css";
 import "phoenix_html"
 import { Socket } from "phoenix"
 import NProgress from "nprogress"
+import 'alpinejs'
 import { LiveSocket } from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
+let liveSocket = new LiveSocket("/live", Socket, {
+  dom: {
+    onBeforeElUpdated(from, to) {
+      if (from.__x) { window.Alpine.clone(from.__x, to) }
+    }
+  },
+  params: { _csrf_token: csrfToken }
+})
 
 // Show progress bar on live navigation and form submits
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
