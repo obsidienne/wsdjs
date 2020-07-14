036e990566fb5c3e957634dd41e4aa33a4a2870d
Claudio Bernardes <bernardes.claudio@gmail.com>
2020-07-14 23:05:52 +0200

    Good pagination

--- lib/wsdjs_web/live/paginate_users_live.ex
+++ lib/wsdjs_web/live/paginate_users_live.ex
@@ -9,7 +9,7 @@ defmodule WsdjsWeb.PaginateUsersLive do
 
   def handle_params(params, _url, socket) do
     page = String.to_integer(params["page"] || "1")
-    per_page = String.to_integer(params["per_page"] || "20")
+    per_page = String.to_integer(params["per_page"] || "9")
 
     paginate_options = %{page: page, per_page: per_page}
     users = Accounts.list_users(paginate: paginate_options)
@@ -21,6 +21,20 @@ defmodule WsdjsWeb.PaginateUsersLive do
       )
 
     {:noreply, socket}
+  end
+
+  defp pagination_link(socket, page, per_page, class, do: content) do
+    live_patch(
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          page: page,
+          per_page: per_page
+        ),
+      class: class,
+      do: content
+    )
   end
 
   defp pagination_link(socket, text, page, per_page, class) do
