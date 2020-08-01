4863b2cde3cca532d25895f9379bbef39c327f20
Claudio Bernardes <bernardes.claudio@gmail.com>
2020-08-02 00:49:17 +0200

    navbar active css

--- lib/wsdjs_web/views/layout_view.ex
+++ lib/wsdjs_web/views/layout_view.ex
@@ -14,14 +14,6 @@ defmodule WsdjsWeb.LayoutView do
     |> Enum.join("")
   end
 
-  def active_class(conn, val) do
-    if Enum.member?(val, view_name(conn)) do
-      "active"
-    else
-      ""
-    end
-  end
-
   # Takes the resource name of the view module and removes the
   # the ending *_view* string.
   defp view_name(conn) do
@@ -41,4 +33,33 @@ defmodule WsdjsWeb.LayoutView do
 
   def current_user_id(nil), do: nil
   def current_user_id(%Wsdjs.Accounts.User{} = user), do: user.id
+
+  @active_link "block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
+  @normal_link "mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+  def navbar_link(conn, text, to: route) do
+    css =
+      if route == Phoenix.Controller.current_path(conn) do
+        @active_link
+      else
+        @normal_link
+      end
+
+    link(text, to: route, class: css)
+  end
+
+
+  @active_pill "px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
+  @normal_pill "px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+  def navbar_pill(conn, text, to: route), do: navbar_pill(conn, text, to: route, class: "")
+
+  def navbar_pill(conn, text, to: route, class: class) do
+    css =
+      if route == Phoenix.Controller.current_path(conn) do
+        @active_pill
+      else
+        @normal_pill
+      end
+
+    link(text, to: route, class: "#{class} #{css}")
+  end
 end
