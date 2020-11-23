f0710ca258ef58d6081887a1fca5b014686023ba
claudio <bernardes.claudio@gmail.com>
2020-11-23 03:10:30 +0100

    Start using tailwindcss2

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-    future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true
-    },
     purge: [
         "../**/*.html.eex",
         "../**/*.html.leex",
@@ -10,24 +6,13 @@ module.exports = {
         "../**/live/**/*.ex",
         "./js/**/*.js"
     ],
-    theme: {
-        aspectRatio: { // defaults to {}
-            'none': 0,
-            'square': [1, 1], // or 1 / 1, or simply 1
-            '16/9': [16, 9],  // or 16 / 9
-            '4/3': [4, 3],    // or 4 / 3
-            '21/9': [21, 9],  // or 21 / 9
-        },
-    },
-    variants: {
-        aspectRatio: ['responsive']
-    },
     plugins: [
-        require('tailwindcss-aspect-ratio'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
         require('tailwindcss-animatecss')({
             classes: ['animate__animated', 'animate__swing'],
             variants: ['responsive', 'hover', 'reduced-motion'],
         }),
-        require('@tailwindcss/ui')
     ]
 };
