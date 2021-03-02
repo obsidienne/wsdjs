43cb955953bf15c9ddc67d6490ce1d4ce2b6e73b
Claudio Bernardes <bernardes.claudio@gmail.com>
2021-03-02 15:18:29 +0100

    Design improvement

--- lib/wsdjs_web/live/charts/chart_component.ex
+++ lib/wsdjs_web/live/charts/chart_component.ex
@@ -1,18 +1,6 @@
 defmodule WsdjsWeb.ChartComponent do
   use WsdjsWeb, :live_component
 
-  def dominante_genre(songs) do
-    songs
-    |> Enum.sort(&(&1 <= &2))
-    |> Enum.group_by(fn x -> x.genre end)
-    |> Enum.map(fn {k, v} -> {k, Enum.count(v)} end)
-    |> Enum.sort(fn {_, v1}, {_, v2} -> v1 >= v2 end)
-    |> Enum.take(3)
-    |> Enum.map(fn {k, v} ->
-      {:safe, "<div>#{k} <span class=\"text-gray-800\">(#{v})</span></div>"}
-    end)
-  end
-
   def count_dj(songs) do
     songs
     |> Enum.map(fn x -> x.user_id end)
