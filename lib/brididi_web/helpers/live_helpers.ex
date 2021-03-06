8ce066bba06ffeeb257354e9fcf27304c2d7f84a
Claudio Bernardes <bernardes.claudio@gmail.com>
2021-03-06 23:50:40 +0100

    Add documentation

--- lib/brididi_web/helpers/live_helpers.ex
+++ lib/brididi_web/helpers/live_helpers.ex
@@ -1,11 +1,13 @@
 defmodule Brididi.LiveHelpers do
   import Phoenix.LiveView
 
-  alias Brididi.Accounts
-
+  @doc """
+  Implements the security defined in the official documentation
+  https://hexdocs.pm/phoenix_live_view/security-model.html
+  """
   def assign_defaults(%{"user_token" => user_token}, socket) do
     socket =
-      assign_new(socket, :current_user, fn -> Accounts.get_user_by_session_token(user_token) end)
+      assign_new(socket, :current_user, fn -> Brididi.Accounts.get_user_by_session_token(user_token) end)
 
     if socket.assigns.current_user.confirmed_at do
       socket
